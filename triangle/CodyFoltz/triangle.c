f9ffda33134bc32117079099f0a6fa56f882d121
Cody Foltz <codablo@gmail.com>
2014-01-13 10:54:45 -0700

    Calculation of sides and angles.

--- triangle/CodyFoltz/triangle.c
+++ triangle/CodyFoltz/triangle.c
@@ -12,13 +12,13 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define NOT_TRIANGLE "not a triangle"
+#define NOT_TRIANGLE "not a triangle\n"
 #define EQUILATERAL "equilateral"
 #define ISOSCELES "isosceles"
 #define SCALENE "scalene"
-#define ACUTE "acute"
-#define RIGHT "right"
-#define OBTUSE "obtuse"
+#define ACUTE "acute/n"
+#define RIGHT "right/n"
+#define OBTUSE "obtuse/n"
 
 /**
  * 
@@ -43,7 +43,7 @@ typedef struct slope{
 char* find_triangle_type(Point point1, Point point2, Point point3);
 Point create_point(char* x, char* y);
 double calculate_distance(Point point1, Point point2);
-int is_legal_triangle(Point point1, Point point2, Point point3);
+int is_illegal_triangle(Point point1, Point point2, Point point3);
 void insure_correct_slope(Slope* slope);
 Slope calculate_slope(Point pointA, Point pointB);
 int are_slopes_equal(Slope slopeA, Slope slopeB);
@@ -80,20 +80,28 @@ int main(int argc, char** argv) {
  */
 char* find_triangle_type(Point point1, Point point2, Point point3)
 {   
-    if(is_legal_triangle(point1, point2, point3)){
+    if(is_illegal_triangle(point1, point2, point3)){
         return NOT_TRIANGLE;
     }
     
-    double side_1_2 = calculate_distance(point1, point2);
-    double side_1_3 = calculate_distance(point1, point3);
-    double side_2_3 = calculate_distance(point2, point3);
+    double side_a = calculate_distance(point1, point2);
+    double side_b = calculate_distance(point1, point3);
+    double side_c = calculate_distance(point2, point3);
+    // cos(A) = (b^2 + c^2 - a^2) / (2bc) 
+    double angle_A = ( (side_b * side_b) + (side_c * side_c) - (side_a * side_a) ) / (2 * side_b * side_c);
+    // cos(B) = (a^2 + c^2 - b^2) / (2ac) 
+    double angle_B = ( (side_a * side_a) + (side_c * side_c) - (side_b * side_b) ) / (2 * side_a * side_c);
+    // cos(C) = (a^2 + b^2 - c^2) / (2ab) 
+    double angle_C = ( (side_a * side_a) + (side_b * side_b) - (side_c * side_c) ) / (2 * side_a * side_b);
+    
+    angle_A = acos(angle_A);
+    angle_B = acos(angle_B);
+    angle_C = acos(angle_C);
     
     //check for equalateral
-    if(side_1_2 == side_1_3 && side_1_2 == side_2_3){
-        return "equilateral acute";
-    }
     
-    return NOT_TRIANGLE;
+    
+    return "Error: Type not found.\n";
     
     
 }
@@ -123,10 +131,12 @@ double calculate_distance(Point point1, Point point2)
 {
     double side = 0;
     
-    //distance formula = sqrt( (x2^2-x1^2) + (y2^2 - y1^2) )
+    //distance formula = sqrt( (x2-x1)^2 + (y2 - y1) )
     
-    double x_side = point2.x * point2.x - point1.x * point1.x;
-    double y_side = point2.y * point2.y - point1.y * point1.y;
+    double x_side = point2.x - point1.x;
+    x_side = x_side * x_side;
+    double y_side = point2.y - point1.y;
+    y_side = y_side * y_side;
     double total = x_side + y_side;
     
     side = sqrt( total );
@@ -142,11 +152,11 @@ double calculate_distance(Point point1, Point point2)
  * @param point3
  * @return 
  */
-int is_legal_triangle(Point point1, Point point2, Point point3){
+int is_illegal_triangle(Point point1, Point point2, Point point3){
     
     //Overlapping points are illegal
     if(has_overlapping_points(point1, point2, point3)){
-        return 0;
+        return 1;
     }
     
     /* rise/run */
@@ -154,7 +164,8 @@ int is_legal_triangle(Point point1, Point point2, Point point3){
     Slope slope_1_3 = calculate_slope(point1, point3);
     
     //Equal slopes means all 3 points form one straight line.
-    return !are_slopes_equal(slope_1_2, slope_1_3);
+    int result = are_slopes_equal(slope_1_2, slope_1_3);
+    return result;
 }
 
 Slope calculate_slope(Point pointA, Point pointB){
@@ -169,7 +180,7 @@ void insure_correct_slope(Slope* slope){
     if(slope->rise < 0 || slope->run < 0){
         slope->is_negative = 1;
         slope->rise = abs(slope->rise);
-        slope.run = abs(slope->run);
+        slope->run = abs(slope->run);
     } else if( slope->rise >= 0 && slope->run >= 0){
         slope->is_negative = 1;
     } else {
