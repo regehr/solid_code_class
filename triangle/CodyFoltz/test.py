437a05be70831249fd3b030ea0969b959049f447
Cody Foltz <codablo@gmail.com>
2014-01-14 11:55:53 -0700

    Finished is-ri tests

--- triangle/CodyFoltz/test.py
+++ triangle/CodyFoltz/test.py
@@ -1,17 +1,43 @@
 #! /usr/bin/python
-#basic idea from Taylor J Stapleton
+#basic idea for function syntax from Taylor J Stapleton
 import subprocess
 
 def test_is_ri():
 	test_successful = True
+
+        #Test 1
         output = subprocess.check_output(['./triangle' , '1' , '1' , '2' , '1', '1' , '2'])
         if(output != "isosceles right\n"):
                 print("isosceles right: failed test 1\n")
 		test_successful = False
-	output = subprocess.check_output(['./triangle' , '0' , '0' , '100', '0', '0', '100'])
+	
+        #Test 2
+        output = subprocess.check_output(['./triangle' , '0' , '0' , '100', '0', '0', '100'])
 	if(output != "isosceles right\n"):
 		print("isosceles right: failed test 2\n")
-		test_sucessful = False
+		test_successful = False
+
+        #Test3
+        output = subprocess.check_output(['./triangle' , '0' , '0' , '5000', '0', '0', '5000'])
+	if(output != "isosceles right\n"):
+		print("isosceles right: failed test 3\n")
+		test_successful = False
+
+#Test4
+        output = subprocess.check_output(['./triangle' , '5000' , '5000' , '5000', '0', '0', '5000'])
+	if(output != "isosceles right\n"):
+		print("isosceles right: failed test 4\n")
+		test_successful = False
+
+
+#Test5
+        output = subprocess.check_output(['./triangle' , '12345678' , '12345678' , '12345678', '0', '0', '12345678'])
+	if(output != "isosceles right\n"):
+		print("isosceles right: failed test 5\n")
+		test_successful = False
+
+
+
 	return test_successful
 
 def main():
