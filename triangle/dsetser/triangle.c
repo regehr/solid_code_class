ee0fb4e85f9571898d2e5beae39280d5756a261f
Daniel Setser <danielsetser@gmail.com>
2014-01-15 20:05:02 -0700

    Fixing problem that caused python scripts to fail, and testing everyone

--- triangle/dsetser/triangle.c
+++ triangle/dsetser/triangle.c
@@ -11,18 +11,27 @@
 
 struct Point
 {
-	long x;
-	long y;
+	long long x;
+	long long y;
 };
 
-long distanceSquared(struct Point a, struct Point b)
+/* Returns the distance squared */
+long long distanceSquared(struct Point a, struct Point b)
 {
 	return ((a.x - b.x)*(a.x - b.x)) + ((a.y - b.y)*(a.y - b.y));
 }
 
-void printType(long a, long long int b, long long int c)
+/* Swaps the value of two long longs */
+void swap(long long* a, long long* b)
 {
-	// Print the first word.
+	long long temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/* Prints the type of triangle given the sides. */
+void printType(long long a, long long int b, long long int c)
+{
 	if(a == b || a == c || b == c)
 	{
 		printf("isosceles ");
@@ -32,22 +41,16 @@ void printType(long a, long long int b, long long int c)
 		printf("scalene ");
 	}
 
-	// Make sure a is the max.
-	long temp;
+	/* Make sure c is the largest angle. */
 	if(b > a && b > c)
-	{
-		temp = b; b = a; a = temp; }
-	else if(c > a && c > b)
-	{	
-		temp = c;
-		c = a;
-		a = temp;
-	}
+		swap(&c, &b);
+	else if(a > c && a > b)
+		swap(&c, &a);
 
-	// Print the second word.
-	if(a - c == b) // b + c == a avoiding overflow
+	/* Print the second word. */
+	if(c - a == b) /* a + b == c avoiding overflow */
 		printf("right\n");
-	else if(a - c < b) // b + c > a avoiding overflow
+	else if(c - a < b) /* a + b > c avoiding overflow */
 		printf("acute\n");
 	else
 		printf("obtuse\n");
@@ -56,9 +59,8 @@ void printType(long a, long long int b, long long int c)
 int main(int argc, char* argv[])
 {
   struct Point a, b, c;
-	long ab, ac, bc;
+	long long ab, ac, bc;
 
-	// Store arguments into points
 	a.x = atoi(argv[1]);
 	a.y = atoi(argv[2]);
 	b.x = atoi(argv[3]);
@@ -66,18 +68,20 @@ int main(int argc, char* argv[])
 	c.x = atoi(argv[5]);
 	c.y = atoi(argv[6]);
 
-	// Check if the points are collinear.
+	/* Check if the points are collinear. */
 	if(((b.y - a.y) * (c.x - b.x)) == ((c.y - b.y) * (b.x - a.x)))
 	{
 		printf("not a triangle\n");
 		return 0;
 	}
 
-	// Find the sides squared.
+	/* Find the length of the sides squared. */
 	ab = distanceSquared(a, b);
 	ac = distanceSquared(a, c);
 	bc = distanceSquared(b, c);
 
 	printType(ab, ac, bc);
+
+	return 0;
 }
 
