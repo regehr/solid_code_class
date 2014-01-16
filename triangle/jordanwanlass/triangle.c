b67b91ee716739981a639b5e0559d8107ec85b8b
Anthony Wanlass <wanlass@lab1-21.eng.utah.edu>
2014-01-16 11:07:25 -0700

    cleaned up code

--- triangle/jordanwanlass/triangle.c
+++ triangle/jordanwanlass/triangle.c
@@ -1,21 +1,31 @@
 #include <stdio.h>
 #include <math.h>
 
-struct triangle {
+struct triangle {	
 	long long A;
 	long long B;
 	long long C;
 };
 
+struct pts {
+	long long ax;
+	long long ay;
+	long long bx;
+	long long by;
+	long long cx;
+	long long cy;
+};
+
+/*	distance between two points, squared */
 long long squaredDistance(long long ax, long long ay, long long bx, long long by) {
 	return pow(bx - ax, 2) + pow(by - ay, 2);
 }
 
 /* 	get the squared length of the sides of the triangle */
-void getSides(struct triangle *triangle, long long points[]){
-	(*triangle).A = squaredDistance(points[0], points[1], points[2], points[3]);
-	(*triangle).B = squaredDistance(points[2], points[3], points[4], points[5]);
-	(*triangle).C = squaredDistance(points[4], points[5], points[0], points[1]);	   
+void getSides(struct triangle *triangle, struct pts pts){
+	(*triangle).A = squaredDistance(pts.ax, pts.ay, pts.bx, pts.by);
+	(*triangle).B = squaredDistance(pts.bx, pts.by, pts.cx, pts.cy);
+	(*triangle).C = squaredDistance(pts.cx, pts.cy, pts.ax, pts.ay);		   
 }
 
 /*	find the largest angle using dot product */
@@ -38,20 +48,20 @@ char getLongSide(struct triangle triangle) {
 
 /* 	determine whether the triangle is right, acute or obtuse by computing the 
 	angle of the longest side */
-char * findTriangle(long long points[], struct triangle triangle) {
-
+char * findTriangle(struct pts pts, struct triangle triangle) {
+	
 	char longSide = getLongSide(triangle);
 	
 	long angle = 0;
 	if(longSide == 'A') {
-		angle = findAngle((points[2] - points[4]), 
-		(points[3] - points[5]), (points[0] - points[4]), (points[1] - points[5]));
+		angle = findAngle((pts.bx - pts.cx), 
+		(pts.by - pts.cy), (pts.ax - pts.cx), (pts.ay - pts.cy));
 	} else if(longSide == 'B') {
-		angle = findAngle((points[2] - points[0]), 
-		(points[3] - points[1]), (points[4] - points[0]), (points[5] - points[1]));
+		angle = findAngle((pts.bx - pts.ax), 
+		(pts.by - pts.ay), (pts.cx - pts.ax), (pts.cy - pts.ay));
 	} else {
-		angle = findAngle((points[0] - points[2]), 
-		(points[1] - points[3]), (points[4] - points[2]), (points[5] - points[3]));
+		angle = findAngle((pts.ax - pts.bx), 
+		(pts.ay - pts.by), (pts.cx - pts.bx), (pts.cy - pts.by));
 	}
 	
 	if(angle == 0) {
@@ -75,37 +85,50 @@ char * findType(struct triangle triangle) {
 
 /* 	determine whether the input points are collinear by calculating the double area of
 	the triangle using cross product */
-int collinearCheck(long long points[6]) {				
-	int bool = ((points[0]*(points[3] - points[5])) +
-				(points[2]*(points[5] - points[1])) + 
-				(points[4]*(points[1] - points[3]))) == 0;
+int collinearCheck(struct pts pts) {								
+	int bool =	((pts.ax * (pts.by - pts.cy)) +
+				(pts.bx * (pts.cy - pts.ay)) +
+				(pts.cx * (pts.ay - pts.by))) == 0;
 					
 	return bool;
 }
 
+/*	initialize all the points in our points struct */
+void setup(struct pts *pts, char *argv[]) {
+	long long inputs[6];
+
+	int i;
+	for(i = 0; i < 6; i++) {
+		inputs[i] = atoll(argv[i + 1]);
+	}
+	
+	(*pts).ax = inputs[0];
+	(*pts).ay = inputs[1];
+	(*pts).bx = inputs[2];
+	(*pts).by = inputs[3];
+	(*pts).cx = inputs[4];
+	(*pts).cy = inputs[5];	
+}
+
 int main(int argc, char *argv[]) {
 	struct triangle triangle = {0};
-	long long points[6];
-	if(argc == 7) {
-	
-		int i;
-		for(i = 0; i < 6; i++) {
-			points[i] = atoll(argv[i + 1]);
-		}
-		
+	struct pts pts = {0};
+
+	if(argc == 7) {		
+		setup(&pts, argv);		
 	} else {
 		printf("not a triangle\n");
 		return 0;
 	}
 
-    getSides(&triangle, points);
-      
-    if(collinearCheck(points)) {
+    getSides(&triangle, pts);
+    
+    if(collinearCheck(pts)) {
     	printf("not a triangle\n");
     	return 0;
     }
     
-     printf("%s %s\n",findType(triangle),findTriangle(points, triangle));
+    printf("%s %s\n",findType(triangle),findTriangle(pts, triangle));
     
 	return 0;
 }
