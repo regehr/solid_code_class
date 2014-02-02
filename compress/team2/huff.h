6d2f836d514f798717b34d20898474080e50b63a
Josh Kunz <joshkunz@me.com>
2014-02-02 14:40:03 -0700

    Changed huffman table API.

--- compress/team2/huff.h
+++ compress/team2/huff.h
@@ -1,23 +1,16 @@
 #ifndef HUFF_H
 #define HUFF_H
 
+/* A Huffman table specifies an encoding for all 256 bytes as a null terminated
+   string: char * HuffmanTable[256] */
+
 /* Compression
 ----------------------------------------------------------------------------- */
 
-struct HuffmanTable;
-
-/* Frees a HuffmanTable struct. Returns 1 on success, 0 otherwise. */
-int huff_freetable(struct HuffmanTable *table);
-
-/* Returns a pointer to a new translation table generated from an array of 256
+/* Returns a new translation table generated from an array of 256
    unsigned long longs representing the number of times the byte index has
    appeared in a file. */
-HuffmanTable *huff_tablefromfreq(unsigned long long freq[256]);
-
-/* Encodes byte using table and places the ASCII representation of the encoding
-   in out. Returns 1 on success, 0 otherwise. The longest possible encoding is
-   255 zeros. */
-int huff_encode(char byte, char out[256], HuffmanTable *table);
+char ** huff_tablefromfreq(unsigned long long freq[256]);
 
 /* Decompression
 ----------------------------------------------------------------------------- */
@@ -30,7 +23,7 @@ int huff_freecontext(struct DecompressionContext *context);
 /* Returns a pointer to a new DecompressionContext struct generated from a
    translation table, which is an array of 256 char *s which are the ASCII
    representations of that byte index's translation. */
-struct DecompressionContext *huff_contextfromtable(char *table[256]);
+struct DecompressionContext *huff_makecontext(char *table[]);
 
 /* Takes the next bit to decode. Returns an unsigned char converted to an int if
    a character is decoded, returns -1 otherwise. If an error occurs, -2 or lower
