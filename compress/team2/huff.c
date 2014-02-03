b64cd3854208c9432f2c166035d122c1d155272a
Josh Kunz <joshkunz@me.com>
2014-02-03 04:23:50 -0700

    Fixed huff.h names in huff.c

--- compress/team2/huff.c
+++ compress/team2/huff.c
@@ -125,7 +125,7 @@ int checktable(char *table[256]) {
 /* Returns a new translation table generated from an array of 256
    unsigned long longs representing the number of times the byte index has
    appeared in a file. Allocates (strlen(i) + 1) * 8 memory for each string. */
-int huff_tablefromfreq(uint64_t freq[256], char *out_table[256]) {
+int huff_make_table(uint64_t freq[256], char *out_table[256]) {
     struct PrioritizedNode nodelist[256];
 
     for (int i = 0; i < 256; i++) {
@@ -164,17 +164,16 @@ int huff_tablefromfreq(uint64_t freq[256], char *out_table[256]) {
 /* Decompression
 ----------------------------------------------------------------------------- */
 
-struct DecompressionContext;
 
-/* Frees a DecompressionContext struct. Returns 1 on success, 0 otherwise. */
-int huff_freecontext(struct DecompressionContext *context);
+/* Frees a decoder struct. Returns 1 on success, 0 otherwise. */
+int huff_free_decoder(struct decoder *);
 
-/* Returns a pointer to a new DecompressionContext struct generated from a
+/* Returns a pointer to a new decoder struct generated from a
    translation table, which is an array of 256 char *s which are the ASCII
    representations of that byte index's translation. */
-struct DecompressionContext *huff_makecontext(char *table[]);
+struct decoder *huff_make_decoder(char *table[]);
 
 /* Takes the next bit to decode. Returns an unsigned char converted to an int if
    a character is decoded, returns -1 otherwise. If an error occurs, -2 or lower
    is returned */
-int huff_decode(int bit, struct DecompressionContext *context);
+int huff_decode(int bit, struct decoder*);
