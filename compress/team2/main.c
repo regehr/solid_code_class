91d58dc057187127d0e1878c7844410994054d5f
Josh Kunz <joshkunz@me.com>
2014-02-02 22:00:33 -0700

    Added huff file validator.

--- compress/team2/main.c
+++ compress/team2/main.c
@@ -2,10 +2,17 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
+#include <stdbool.h>
 #include "huff.h"
 
-static const int ERROR_EXIT = 255;
-static const int SUCCESS_EXIT = 0;
+const int ERROR_EXIT = 255;
+const int SUCCESS_EXIT = 0;
+
+const char HUFF_MAGIC[] = "HUFF";
+const char HUFF_EXT[] = ".huff";
+
+const int HUFF_MAGICLEN = sizeof(HUFF_MAGIC) - 1;
+const int HUFF_EXTLEN = sizeof(HUFF_EXT) - 1;
 
 void usage(FILE * to) {
     fputs("usage: huff [-t | -c | -d] FILE\n"
@@ -16,9 +23,41 @@ void usage(FILE * to) {
           "         then a compression table is generated and printed.\n", to);
 }
 
-int compress(FILE * file, char * filename) { return ERROR_EXIT; }
-int decompress(FILE * file, char * filename) { return ERROR_EXIT; }
-int table(FILE * file) { return ERROR_EXIT; }
+/* Returns 'true' when the given file has all the markings of a huff file,
+ * returns 'false' when the given file lacks a marker. */
+bool is_huff(FILE * file, char * filename) {
+    /* make sure the name is at least as long as HUFF_EXT */
+    size_t name_length = strlen(filename);
+    if (name_length < HUFF_EXTLEN) { return false; }
+
+    /* make sure the filename ends with HUFF_EXT */
+    char * ext_start = &filename[name_length - HUFF_EXTLEN];
+    if (! (strcmp(ext_start, HUFF_EXT) == 0)) { return false; }
+
+    /* prepare a buffer for reading. */
+    char magic[HUFF_MAGICLEN];
+    memset(magic, 0, HUFF_MAGICLEN);
+
+    /* check if the given file has the HUFF magic number */
+    int items = fread(magic, HUFF_MAGICLEN, 1, file);
+    if (items < 1 || ! (strncmp(magic, HUFF_MAGIC, HUFF_MAGICLEN) == 0)) {
+        return false;
+    }
+
+    return true;
+}
+
+int compress(FILE * file, char * filename) { 
+    return ERROR_EXIT; 
+}
+
+int decompress(FILE * file, char * filename) { 
+    return ERROR_EXIT; 
+}
+
+int table(FILE * file, char * filename) { 
+    return ERROR_EXIT; 
+}
 
 int main(int argc, char *argv[]) {
     int exit_code = ERROR_EXIT;
@@ -43,7 +82,7 @@ int main(int argc, char *argv[]) {
     } else if (strcmp(argv[1], "-d") == 0) {
         exit_code = decompress(input, argv[2]);
     } else if (strcmp(argv[1], "-t") == 0) {
-        exit_code = table(input);
+        exit_code = table(input, argv[2]);
     } else {
         usage(stderr); 
     }
