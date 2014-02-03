8bb5994bdc971fec8aa90fb20649254e19b7b1d8
Furano <dominicfurano@gmail.com>
2014-02-03 13:03:52 -0700

    message

--- compress/team1/huff.c
+++ compress/team1/huff.c
@@ -14,6 +14,11 @@ int main(int argc, const char *argv[])
     
     char *huff_table[256];
     uint64_t frequencies[256];
+
+    // I had that function setup initially to accept a filestream,
+// but I think it would be a better idea to keep the file stuff separate
+// in here. Also that function takes an array of node pointers which would
+// also break the modularity.
     
     
     // Andres, if you pass me a sorted array of frequencies and an empty char * array
