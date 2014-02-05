cf1f412b6266c534c8527ddd61535892bef12561
lgao <gao_lynn@hotmail.com>
2014-02-05 13:17:01 -0700

    added huffman tree for compression

--- compress/group4/huff.c
+++ compress/group4/huff.c
@@ -7,6 +7,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define CHAR_COUNT = 257 // number of characters
+#define CHAR_EOF = 256 // represents end of file
+
+
+/* Returns true if the file is a huff file, else otherwise */
+bool huff_file(char* filename)
+{
+  char* ext = strrchr(filename, '.');
+  char* result = ext+1;
+ 
+    
+  return false;
+} 
+
+    
+/*
+ * Dump the compression table for a file
+ */
+void display_table (char* file, struct frequency table[])
+{
+  //check if the file is a huff file
+  //bool isHuff = huff_file(file);
+  // if not a huff file, compute compression table    
+  int i;
+  for(i=0; i < 256; i++)
+    {
+      printf("%c %i\n", table[i].character, table[i].count);
+    }
+ }
+
 
 int main (int argc, char *argv[]) {
     char *file = "";
@@ -29,14 +64,11 @@ int main (int argc, char *argv[]) {
         case DECOMPRESS:
         break;
         case TABLE:
-            build_table(file, table); 
+	    build_table(file, table); 
+	    display_table(file, table);
         break;
         case UNHANDLED:
         break;
-    }
-
-    for (i = 0; i < 256; i++) {
-        printf("%c: %d\n", table[i].character, table[i].count);
     }
 
     return 0;
