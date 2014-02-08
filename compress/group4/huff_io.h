26a07987f4c26a818539633b92922ca5ec13db1d
Jonathon <jmcdonald.ut@gmail.com>
2014-02-08 15:37:56 -0700

    Styling update

--- compress/group4/huff_io.h
+++ compress/group4/huff_io.h
@@ -19,35 +19,31 @@ struct frequency {
 };
 
 // represents nodes in the Huffman tree
-typedef struct treeNode treeNode;
-struct treeNode {
-  struct treeNode* parent;
-   struct treeNode* left;  // 0
-   struct treeNode* right; // 1
-   int weight; // frequency of character in file
-   int current; //ascii value of character (int)char
-   }; 
+typedef struct tree_node tree_node;
+struct tree_node {
+    tree_node *parent;
+    tree_node *left;  // 0
+    tree_node *right; // 1
+    int weight; // frequency of character in file
+    int current; //ascii value of character (int)char
+}; 
 
 // represents a priority queue for nodes in the tree
-struct pqNode {
-  int priority;
-  struct pqNode* next; // a pointer to the next node in the queue
-  struct treeNode content;
-  };
-
+struct pq_node {
+    int priority;
+    struct pq_node *next; // a pointer to the next node in the queue
+    struct tree_node content;
+};
 
 
 flags parse_args (char *[], char **);
 int compare (const void *, const void *);
 void build_table (char *, struct frequency []);
-
-void dump_table(char *, struct frequency[]);
-void traverse_tree(struct treeNode);
-struct pqNode makePQ(struct frequency[]);
-struct treeNode build_tree(struct pqNode);
-void enqueue(struct pqNode*, struct pqNode*);
-struct treeNode dequeue(struct pqNode*);
-int compareTo(struct treeNode*, struct treeNode*);
-void printTree(struct treeNode);
-
-
+void dump_table (char *, struct frequency[]);
+void traverse_tree (tree_node);
+struct pq_node make_pq (struct frequency[]);
+struct tree_node build_tree (struct pq_node);
+void enqueue (struct pq_node *, struct pq_node *);
+struct tree_node dequeue (struct pq_node*);
+int compare_to (tree_node *, tree_node *);
+void print_tree (tree_node);
