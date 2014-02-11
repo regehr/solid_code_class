ef1845f8241df1fc7e941f6393fd2ee383fe1298
Furano <dominicfurano@gmail.com>
2014-02-11 00:58:57 -0700

    nvmd

--- compress/team1/huff_io.c
+++ compress/team1/huff_io.c
@@ -190,10 +190,8 @@ static void flush_bits (FILE* file)
         write_bit (0, file);
     
     // Write final byte.
-    if (current_bit == -1){
-       if (fwrite (&bit_buffer, 1, 1, file) != 1){
+    if (fwrite (&bit_buffer, 1, 1, file) != 1){
         write_body_error();
-       }
     }
 }
 
