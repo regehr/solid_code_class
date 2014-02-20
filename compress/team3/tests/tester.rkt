9754eb4960cb57b841ee2b14f34251a03e29abe1
Josh Kunz <joshkunz@me.com>
2014-02-20 15:29:03 -0700

    Last testing framework refactor.
        * Added framework
        * Fixed 'make test' target

--- compress/team3/tests/tester.rkt
+++ compress/team3/tests/tester.rkt
@@ -2,22 +2,13 @@
 
 (require (only-in racket/generator generator yield)
          (only-in rnrs/base-6 assert))
+
 (require "term-colors.rkt")
-
-
-(define printf-info (make-printfs
-                      (make-color 'white)))
-(define format-fail (make-formats
-                     (make-color 'red)))
-(define format-succ (make-formats
-                     (make-color 'green)))
+(require "framework.rkt")
 
 ; The exit code proper huff implementations are supposed to fail with.
-(define *failure-code* 255)
-(define *success-code* 0)
-
-(define *SUCCS* #f)
-(define *FAILS* #t)
+(define *failure* 255)
+(define *success* 0)
 
 ; HURL file magic number
 (define *huff-magic* (string->bytes/utf-8 "HUFF"))
@@ -32,7 +23,7 @@
 ; (helper for hex-string->bytes)
 (define (hex-chars->bytes chars [bytes '()])
  (cond
-  [(= (length chars) 0) (list->bytes bytes)]
+  [(null? chars) (list->bytes bytes)]
   [(= (length chars) 1) (error "Could not translate odd-length string.")]
   [else
    (let*-values ([(head tail) (split-at chars 2)]
@@ -59,104 +50,22 @@
 ; Return the byte representation of the supplied table.
 (define (table->bytes table)
  (string->bytes/utf-8 
-  (string-join (valid-tree) "\n" #:after-last "\n"))
-)
-
-; Write the supplied test-generator to the given port by generating
-; test outputs.
-(define (write-test test-generator to-file)
- (let ([writer (lambda (bytes) 
-               (write-bytes bytes to-file))])
-  (map writer (sequence->list 
-               (in-producer test-generator 'stop)))
- )
-)
-
-
-; Run a test that builds a test-file using 'test-generator', passes test-file
-; to the test program, and then checks for the proper error code.
-(define (run-test test-generator flag 
-                  #:bin [bin "huff"] 
-                  #:temp-file [temp-file "test.huff"]
-                  #:echo [echo? #f])
- ; Write out our test file.
- (let ([out-file (open-output-file temp-file #:exists 'truncate)])
-  (write-test test-generator out-file)
-  (close-output-port out-file)
- )
- (when echo? (printf "$ ~a ~a ~a\n" bin flag temp-file))
- ; Run the program as a subprocess 
- (let-values ([(proc proc-stdout proc-stdin proc-stderr)
-               (subprocess #f #f #f bin flag temp-file)])
-  (begin0
-   ; Collect the process's output and status code for return
-   (let ([proc-output (port->bytes proc-stdout)]
-         [proc-error (port->bytes proc-stderr)])
-    (subprocess-wait proc)
-    (list (subprocess-status proc) proc-output proc-error)
-   )
-   ; Cleanup the communication channels.
-   (close-output-port proc-stdin)
-   (close-input-port proc-stdout)
-   (close-input-port proc-stderr)
-  )
- )
-)
-
-; General test result checking code. Evaluates to #t if the test passed,
-; and evaluates to #f if the test failed.
-(define (check-test test-name test-output fail? . test-checks)
- (let-values ([(code stdout stderr) (apply values test-output)])
-  (let* ([expected-code (if fail? *failure-code* *success-code*)]
-         [correct-status? (= code expected-code)]
-         [check-runner (lambda (check) (check stdout stderr))]
-         [check-results (map check-runner test-checks)]
-         [checks-passed? (andmap car check-results)])
-   (if (and correct-status? checks-passed?)
-    (begin0 #t
-     (printf "~a  ~a\n" (format-succ "OK") test-name))
-    (begin0 #f ; else
-     (printf "~a  ~a\n" (format-fail "FAILED") test-name)
-     (printf "    Error code: ~a (expected ~a)\n" code expected-code)
-     (let ([make-lines (lambda (outputs)
-                        (string-join 
-                         (map (lambda (v) (string-append "    " v)) outputs)
-                         "\n"))])
-      (display (string-join 
-                (map (lambda (v) (make-lines (cdr v))) check-results) 
-                "\n" #:after-last "\n"))
-     )
-    )
-   )
-  )
- )
-)
-
-; Runs all supplied tests. 
-(define (test-all . tests)
- (let*-values ([(environment predicates) (split-at (car tests) 5)]
-               [(test-name generator flag file-name succeeds) 
-                (apply values environment)])
-  (if (apply check-test (append (list test-name
-       (run-test generator flag #:temp-file file-name) succeeds) predicates))
-   (delete-file file-name)
-   (let ([saved-file-name 
-          (string-append (string-downcase
-           (string-replace (string-normalize-spaces test-name) " " "-"))
-           ".input")])
-    (rename-file-or-directory file-name saved-file-name #t)
-    (printf "Triggering input saved into: ~a\n\n" saved-file-name)
-   )
-  )
- )
- (unless (= (length tests) 1) 
-  (apply test-all (cdr tests)))
+  (string-join table "\n" #:after-last "\n"))
 )
 
 ; ***** CHECKERS ***** ;
 
+(define (expect-code expected-code)
+ (lambda (code stdout stderr)
+  (if (not (= code expected-code))
+   (list #f
+    (format "Bad error code: ~a (expected ~a)" code expected-code))
+   (list #t))
+ )
+)
+
 ; Prints out stderr, always succeeds
-(define (output-printer stdout stderr)
+(define (print-output code stdout stderr)
  (list #t 
   (string-append (style "stdout" (make-color 'white)) "\n"
    (string-trim (bytes->string/utf-8 stdout)))
@@ -167,7 +76,27 @@
 
 ; ***** TESTS ******* ;
 
-(define empty-huff
+(define (no-magic)
+ (generator ()
+  (yield (length-as-bytes 1))
+  (yield (table->bytes (valid-tree)))
+  (yield (hex-string->bytes "80"))
+  (yield 'stop)
+ )
+)
+
+(define (bad-table-entry)
+ (generator ()
+  (yield *huff-magic*)
+  (yield (length-as-bytes 1))
+  (yield (table->bytes
+          (append (cdr (valid-tree)) '("2"))))
+  (yield (hex-string->bytes "80"))
+  (yield 'stop)
+ )
+)
+
+(define (empty-huff)
  (generator ()
   (yield *huff-magic*)
   (yield (length-as-bytes 0))
@@ -176,7 +105,17 @@
  )
 )
 
-(define bad-length
+(define (normal-huff)
+ (generator ()
+  (yield *huff-magic*)
+  (yield (length-as-bytes 1))
+  (yield (table->bytes (valid-tree)))
+  (yield (hex-string->bytes "80"))
+  (yield 'stop)
+ )
+)
+
+(define (bad-length)
  (generator ()
   (yield *huff-magic*)
   (yield (length-as-bytes 1))
@@ -185,10 +124,38 @@
  )
 )
 
-
-(test-all
- (list "Empty huff file" 
-  empty-huff "-d" "t.huff" *SUCCS* output-printer)
- (list "Incorrect huff file length" 
-  bad-length "-d" "t.huff" *FAILS* output-printer)
+(define (test-all . tests)
+ (unless (null? tests)
+  (apply test (car tests))
+  (apply test-all (cdr tests))
+ )
 )
+
+(define (huff-decompress name file-generator expected-code 
+         #:file-base [file-base "t"] . checkers)
+ (let ([huff-name (string-append file-base ".huff")])
+  (test name
+   (list (build-file file-generator huff-name))
+   (run-binary "huff" "-d" huff-name)
+   (append (list (expect-code expected-code)) checkers)
+   #:finally (list (rm file-base)))
+ )
+)
+
+(begin0 (void)
+(huff-decompress "Simple decompression"
+ (normal-huff) *success* print-output)
+(huff-decompress "Empty huff file" 
+ (empty-huff) *success* print-output)
+(huff-decompress "Incorrect length in huff file"
+ (bad-length) *failure* print-output)
+(huff-decompress "No magic number in huff file"
+ (no-magic) *failure* print-output)
+(huff-decompress "2 in translation table"
+ (bad-table-entry) *failure* print-output)
+(test "Wrong extension"
+ (list (build-file (normal-huff) "t"))
+ (run-binary "huff" "-d" "t")
+ (list (expect-code *failure*) print-output))
+)
+
