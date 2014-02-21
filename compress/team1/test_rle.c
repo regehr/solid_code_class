308ea22481c1080b16a9df1735abb9928f4bcdcb
Hutch Jorgensen <armage17@hotmail.com>
2014-02-20 23:58:57 -0700

    Added small manual tester to run against my encode_rle method

---
+++ compress/team1/test_rle.c
@@ -0,0 +1,22 @@
+#include "rle.h"
+
+int main(int argc, char* argv[])
+{
+  FILE *fp;
+  fp = fopen(argv[1], "r");
+  if(fp == NULL) {
+    fprintf(stderr, "ERROR: Could not open file\n");
+    exit(255);
+  }
+
+  char* bytes;
+  unsigned long long len;
+  encode_rle(fp, bytes, &len);
+
+  int i;
+  for(i = 0; i < len; i++) {
+    printf("hex: %x for index: %d\n", bytes[i], i);
+  }
+
+  return 0;
+}
