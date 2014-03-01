regehr/solid_code_class

commit 150fe3e1d5144c917364140e5d6193c13bd56bae

Check the return value of xfread

compress/team3/decoder.c
@@ -71,7 +71,9 @@ unsigned long long huffmanEncodingsFromFile(FILE *file, char encodings[32768])
     rewind(file);
     //get the first 4 header characters
     char magic[4];
-    xfread(magic, 4, sizeof(char), file);
+    size_t read = xfread(magic, 4, sizeof(char), file);
+    /* We should have already done size checks. */
+    assert(read == 4);
 
     //make sure it says HUFF
     if(!(magic[0] == 'H' &&

commit a3b18c8361fcb75acb604b972231669fb0772621

Fix bugs introduced by 150fe3e1d5 and 2dc2bcddcc

compress/team3/decoder.c
@@ -73,7 +73,7 @@ unsigned long long huffmanEncodingsFromFile(FILE *file, char encodings[32768])
     char magic[4];
     size_t read = xfread(magic, 4, sizeof(char), file);
     /* We should have already done size checks. */
-    assert(read == 4);
+    assert(read == 1);
 
     //make sure it says HUFF
     if(!(magic[0] == 'H' &&

compress/team3/rhuff.c
@@ -93,8 +93,8 @@ eMode getModeOfOperation(int argc, char *argv[3])
 eFileCode ReadHeader(FILE* pFile, unsigned long long* pHuffmanSize)
 {
     int i = 0;
-    *pHuffmanSize = 0;
     int c;
+    *pHuffmanSize = 0;
     char* header = "HURL";
 
     assert(pFile != NULL);
@@ -147,8 +147,6 @@ eFileCode GetTableForGeneric(unsigned* pFrequencies, huffResult resultArray[256]
  */
 eFileCode GenerateFrequenciesForGeneric(FILE* pFile, unsigned pOutFrequencies[256])
 {
-    int c;
-
     assert(pFile != NULL);
 
     while (true)
@@ -158,7 +156,7 @@ eFileCode GenerateFrequenciesForGeneric(FILE* pFile, unsigned pOutFrequencies[25
 
         if (read == 0) return FILE_SUCCESS;
 
-        pOutFrequencies[byte]++;
+        pOutFrequencies[(int)byte]++;
     }
 }
 
@@ -286,7 +284,9 @@ eFileCode GenerateTableAndCompressOrDecompress(eMode huffmanMode, const char *fi
             }
             huffResult resultArray[ENTRIES];
             fileCode = GetTableForGeneric(pFrequencies, resultArray);
-            FILE *pNewFile = fopen(strcat(fileName, ".hurl"), "w+");
+            char newFileName[strlen(fileName) + 6];
+            strcpy(newFileName, fileName);
+            FILE *pNewFile = fopen(strcat(newFileName, ".hurl"), "w+");
 
             if (pNewFile == NULL)
             {