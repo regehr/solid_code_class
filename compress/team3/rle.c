regehr/solid_code_class

commit 50c6731927820234dbea0d4b7bb2fa80ac32b5ff

Cleaned up team3's RLE implementation.

compress/team3/rle.c
@@ -31,23 +31,21 @@ void rle_encode(FILE *input, FILE *output)
     assert(output);
 
     char encoding_value = 0;
-    char encoding_length = 0;
+    int encoding_length = -1;
 
-    while (true)
+    /* Read out a byte. If the number of bytes read isn't 1, then we're at
+       the EOF because xfread will kill the program if there was actually an
+       error. */
+    char byte = 0;
+    while (xfread(&byte, 1, 1, input) == 1) 
     {
-        /* Read out a byte. If the number of bytes read isn't 1, then we're at
-           the EOF because xfread will kill the program if there was actually an
-           error. */
-        char byte;
-        size_t num_read = xfread(&byte, 1, 1, input);
-        if (num_read != 1) break;
-
         /* For each bit index... */
         for (int i = 0; i < 8; i++)
         {
             char bit = (byte >> (7 - i)) & 0x1;
-            if (bit != encoding_value)
-            {
+            if (encoding_length == -1) encoding_value = bit;
+
+            if (bit != encoding_value) {
                 rle_encode_write(encoding_value, encoding_length, output);
                 encoding_value = bit;
                 encoding_length = 1;
@@ -55,18 +53,20 @@ void rle_encode(FILE *input, FILE *output)
             }
 
             encoding_length++;
-            /* If we've reached the max length possible, write out the byte. */
-            if (encoding_length != MAX_RUN_LENGTH) continue;
 
-            rle_encode_write(encoding_value, encoding_length, output);
-            encoding_value = 0;
-            encoding_length = 0;
+            /* If we've reached the max length possible, write out the byte. */
+            if (encoding_length != MAX_RUN_LENGTH) {
+                encoding_value = 0;
+                encoding_length = 0;
+                rle_encode_write(encoding_value, encoding_length, output);
+            }
         }
     }
 
-    if (encoding_length == 0) return;
-
-    rle_encode_write(encoding_value, encoding_length, output);
+    /* flush the remaining encoding if one exists */
+    if (encoding_length != 0) {
+        rle_encode_write(encoding_value, encoding_length, output);
+    }
 }
 
 void rle_decode(FILE *input, FILE *output)
@@ -74,27 +74,27 @@ void rle_decode(FILE *input, FILE *output)
     char decode_buffer = 0;
     char decode_index = 0;
 
-    while (true)
+    char byte = 0;
+    while (xfread(&byte, 1, 1, input) == 1)
     {
-        char byte;
-        size_t num_read = xfread(&byte, 1, 1, input);
-        if (num_read != 1) break;
-
         for (int i = 0; i < rle_length(byte); i++)
         {
             decode_buffer |= rle_bit(byte) << (7 - decode_index);
             decode_index++;
 
-            if (decode_index != 8) continue;
-
-            xfwrite(&decode_buffer, 1, 1, output);
-            decode_buffer = 0;
-            decode_index = 0;
+            /* When we've read a whole byte, flush the buffer. */
+            if (decode_index == 8) { 
+                xfwrite(&decode_buffer, 1, 1, output);
+                decode_buffer = 0;
+                decode_index = 0;
+            }
         }
     }
 
-    if (decode_index == 0) return;
-
-    fprintf(stderr, "Bad .hurl file.\n");
-    exit(-1);
+    /* If we stopped decoding in the middle of a file, that file is
+     * poorly formatted. */
+    if (decode_index == 0) {
+        fprintf(stderr, "Bad .hurl file.\n");
+        exit(-1);
+    }
 }

.gitignore
@@ -1,18 +1,17 @@
 *.o
-compress/team1/huff
 *.dot
 *.rhuff
 *.swp
 *.swo
 *~
 *.txt
+*.pbm
+
+*huff
+*rhuff
 
 compress/team1/copy_to_ubuntu.sh
-
 compress/team1/kern
-
 compress/team1/mach_kernel
-
 compress/team2/kernel
 
-*.pbm

compress/team3/Makefile
@@ -3,7 +3,7 @@
 RKT = racket
 
 CC = clang
-CFLAGS = -std=c99 -O3
+CFLAGS = -std=c99 -O3 -Wall -Wextra -Wno-unused -Wno-unused-parameter
 
 objects = syscalls.o encoder.o decoder.o decodedOutputFileWriter.o \
 encodedOutputFileWriter.o rhuff.o

compress/team3/tests/framework.rkt
@@ -95,6 +95,18 @@
  )
 )
 
+; Copies the given source file to the given destination file.
+(define (cp source dest #:exists [exists 'truncate]) (thunk
+ (let ([input (open-input-file source #:mode 'binary)]
+       [output (open-output-file dest #:mode 'binary #:exists exists)])
+  (copy-port input output)
+  (close-output-port output)
+  (close-input-port input)
+ )
+ (list)
+))
+
+
 ; Returns a function that when executed writes the bytes generated by
 ; 'test-generator' into 'to-file' and returns functions neccicary to cleanup
 ; the file.

compress/team3/tests/tester.rkt
@@ -102,6 +102,25 @@
  )
 ))
 
+;(define (files-match? A B #:hexdiff [hexdiff? #t])
+; (if
+;  (let ([port_a (open-input-file A #:mode 'binary)]
+;        [port_b (open-input-file B #:mode 'binary)])
+;   (begin0 
+;    (and
+;     (for ([ab port_a] [bb port_b] #:finally (not (= ab bb)))
+;      (= ab bb))
+;     (= (sequence-length port_a) (sequence-length port_b))
+;    )
+;    (close-input-port port_a)
+;    (close-input-port port_b)
+;   )
+;  )
+; ) #t
+; (when hexdiff?)
+;  (
+;)
+
 (define (expect-code expected-code)
  (lambda (results)
   (let ([exit-code (car (hash-ref results 'status))])
@@ -215,6 +234,24 @@
    #:finally (list (rm file-base)))
  )
 )
+
+
+;(define (huff-cycle name file-sequence file-base . checkers)
+; (let ([huff-name (string-append file-base *huff-hext*)])
+;  (test (string-append name ": Compress")
+;   (list (build-file file-sequence file-base) 
+;         (cp file-base (string-append file-base ".bak")))
+;   (run-binary *huff-bin* "-c" file-base)
+;   (append (list (expect-code *success*) (file-exists huff-name)) checkers)
+;  )
+;  (test (string-append name ": Decompress")
+;   (list)
+;   (run-binary *huff-bin* "-d" huff-name)
+;   (append (list (expect-code *success*) (file-exists huff-name)) checkers)
+;  )
+; )
+;)
+
 
 (begin0 (void)
 (huff-decompress "Simple decompression"

commit bd23a051e6af9b80e78dc1833d101b0139be7dad

Fix errors introduced by 50c6731927

compress/team3/rle.c
@@ -17,10 +17,10 @@ static char rle_length(char byte)
 static void rle_encode_write(char value, char length, FILE *output)
 {
     assert(value == 0 || value == 1);
+    assert(length > 0);
     assert(length <= MAX_RUN_LENGTH);
     assert(output);
 
-    if (length == 0) return;
     char byte = (value << 7) | length;
     xfwrite(&byte, 1, 1, output);
 }
@@ -30,20 +30,25 @@ void rle_encode(FILE *input, FILE *output)
     assert(input);
     assert(output);
 
-    char encoding_value = 0;
-    int encoding_length = -1;
+    char encoding_value = -1;
+    int encoding_length = 0;
 
     /* Read out a byte. If the number of bytes read isn't 1, then we're at
        the EOF because xfread will kill the program if there was actually an
        error. */
     char byte = 0;
-    while (xfread(&byte, 1, 1, input) == 1) 
+    while (xfread(&byte, 1, 1, input) == 1)
     {
         /* For each bit index... */
         for (int i = 0; i < 8; i++)
         {
             char bit = (byte >> (7 - i)) & 0x1;
-            if (encoding_length == -1) encoding_value = bit;
+
+            if (encoding_value == -1) {
+                encoding_value = bit;
+                encoding_length = 1;
+                continue;
+            }
 
             if (bit != encoding_value) {
                 rle_encode_write(encoding_value, encoding_length, output);
@@ -55,10 +60,10 @@ void rle_encode(FILE *input, FILE *output)
             encoding_length++;
 
             /* If we've reached the max length possible, write out the byte. */
-            if (encoding_length != MAX_RUN_LENGTH) {
-                encoding_value = 0;
-                encoding_length = 0;
+            if (encoding_length == MAX_RUN_LENGTH) {
                 rle_encode_write(encoding_value, encoding_length, output);
+                encoding_value = -1;
+                encoding_length = 0;
             }
         }
     }
@@ -83,7 +88,7 @@ void rle_decode(FILE *input, FILE *output)
             decode_index++;
 
             /* When we've read a whole byte, flush the buffer. */
-            if (decode_index == 8) { 
+            if (decode_index == 8) {
                 xfwrite(&decode_buffer, 1, 1, output);
                 decode_buffer = 0;
                 decode_index = 0;
@@ -91,9 +96,8 @@ void rle_decode(FILE *input, FILE *output)
         }
     }
 
-    /* If we stopped decoding in the middle of a file, that file is
-     * poorly formatted. */
-    if (decode_index == 0) {
+    /* If our bit count isn't divisible by 8, then we have a bad .hurl file */
+    if (decode_index != 0) {
         fprintf(stderr, "Bad .hurl file.\n");
         exit(-1);
     }