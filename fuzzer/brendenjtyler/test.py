103058a825d5138c00e0c625231235ec489697a2
Brenden Tyler <brendenjtyler@gmail.com>
2014-03-23 19:18:00 -0600

    First commit of fuzzer
    Just built my fuzzer. It's not much to look at but I did it.

---
+++ fuzzer/brendenjtyler/test.py
@@ -0,0 +1,38 @@
+import subprocess
+import sys
+from random import randint
+
+#open up a file to write to
+writeTo = open("hello.c", "w")
+
+#write the headers for the c program
+writeTo.write("#include <stdio.h>\r\n")
+writeTo.write("\r\n")
+writeTo.write("int main (int argc, char **argv)\r\n")
+writeTo.write("{\r\n")
+
+#write the printf lines (right now we're doing the highest 1000 ints and trying to print them as chars)
+for printLines in range(2**32-1000, 2**32):
+    writeTo.write("printf(\"%c\\n\", " + str(printLines) + ");\n")
+
+#close up c program
+writeTo.write("}\r\n")
+
+#close the file
+writeTo.close()
+
+#compile it (with warning turned off because the compiler complains about int and chars and such)
+subprocess.call(["/usr/local/musl/bin/musl-gcc", "hello.c", "-w", "-o", "hello"])
+
+#run it
+p = subprocess.Popen(["./hello", "1", "2", "3"], stdout=subprocess.PIPE)
+
+#get the output
+out, err = p.communicate()
+
+#turn the output into a list (removing empty entries)
+listOut = filter(None, out.split('\n'))
+
+#print out the list
+print(listOut)
+
