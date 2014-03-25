3c96edd1ba75a47ca4aab8b749d4e459729c42b3
lgao <gao_lynn@hotmail.com>
2014-03-25 11:27:32 -0600

    added more tests

--- fuzzer/lynntesia/fuzzer.py
+++ fuzzer/lynntesia/fuzzer.py
@@ -13,11 +13,58 @@ def small_fuzz():
 	f.close()
 
 
+def string_fuzz():
+	f = open("strings.c", "w")
+	f.write("#include <stdio.h>\r\n")
+	f.write("int main(int argc, const char *argv[] {\r\n")
+	f.write("printf('string test');\n")
+	
+	n = 100000
+	test_words = large_string(n)
+	f.write("printf(\"%s\\n\", " + test_words + ");\n")
+	# close file
+	f.write("}\r\n")
+	call("strings.c", "strings")
+	p = subprocess.Popen("./strings", shell=True, stdout=subprocess.PIPE)
+	out, err = p.communicate()
+	listOut = filter(None, out.split('\n'))
+	
+def large_string(n):
+	ret = ""
+	for i in xrange(n):
+		ret += random.choice(string.letters)
+	return ret
 
+def rand_int_fuzz():
+	f = open("randints.c", "w")
+	f.write("#include <stdio.h>\r\n")
+	f.write("int main(int argc, const char *argv[] {\r\n")
+	f.write("printf('integer test');\n")
+	n = 10240
+	test_ints = random_bigints(n)
+	f.write("printf(\"%d\\n\", " + str(test_ints) + ");\n")
+	f.write("}\r\n")
+	call("randints.c", "randints")
+	p = subprocess.Popen("./randints", shell=True, stdout=subprocess.PIPE)
+	out, err = p.communicate()
+	listOut = filter(None, out.split('\n'))
+	
+def random_bigints(n):
+	return random.getrandbits(n)
 
+def write_headers(f):
+	f.write("#include <stdio.h>\r\n")
+	f.write("int main(int argc, const char *argv[] {\r\n")
+	f.write("printf(stringtest\n);\n")
+	return f
+
+def call(filename, name):
+	subprocess.call(["/usr/local/musl/bin/musl-gcc", "-static", filename, "-w", "-o", name])
+	
 
 def main():
-	small_fuzz()
+	#string_fuzz()
+	rand_int_fuzz()
 	return 0
 
 if __name__ == "__main__":
