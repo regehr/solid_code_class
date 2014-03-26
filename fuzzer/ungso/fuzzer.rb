bb7498ac33fe6cb69223d626ede808ad8e10da0d
ungso <yellowleaves11@gmail.com>
2014-03-26 08:32:28 -0600

    Added gcc and musl compare

--- fuzzer/ungso/fuzzer.rb
+++ fuzzer/ungso/fuzzer.rb
@@ -4,16 +4,42 @@ require "test/unit"
  
 class TestPrintf < Test::Unit::TestCase
 
+
   def setup    
-    system('/usr/local/musl/bin/musl-gcc hello.c')
+    system('/usr/local/musl/bin/musl-gcc hello.c -o m') 
+    system('gcc hello.c -o g')
+
   end
 
   def test_simple
 
     s = "hello\n"
-    stdout = IO.popen('./a.out '+s)
+    stdout = IO.popen('./m -s '+s)
     assert_equal(s, stdout.readlines[0])
     
   end
  
+  def test_gcc
+
+    s = "hello\n"
+    stdout1 = IO.popen('./m -s '+s)
+    stdout2 = IO.popen('./g -s '+s)
+    assert_equal(stdout1.readlines[0], stdout2.readlines[0])
+    
+  end
+
+  def test_int
+
+    exp = 31
+    beg = (-1) * (2**exp)
+    last = (2**exp) - 1
+    (beg..last).step(2**21) do |beg|
+      stdout1 = IO.popen('./m -i ' +beg.to_s)
+      stdout2 = IO.popen('./g -i '+beg.to_s)
+      assert_equal(stdout1.readlines[0], stdout2.readlines[0])
+      end
+  end
+
+
+
 end
