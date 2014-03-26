7478ee8d38d84d46c94d4f0052c46f2a9ce00144
Thomas Gonsor <u0677820@utah.edu>
2014-03-26 13:15:44 -0600

    Updated fuzzer to now fuzz strings and diff all file outputs. Just need coverage and better testing now.

--- fuzzer/uLDominator/fuzzer.py
+++ fuzzer/uLDominator/fuzzer.py
@@ -1,6 +1,8 @@
 import subprocess
 import sys
-from random import randint
+import random
+import decimal
+import string
 
 # %i or %d = int
 # %c = char
@@ -17,19 +19,13 @@ def testIntegers():
 	writer.write("int main (int argc, const char *argv[])\r\n")
 	writer.write("{\r\n")
 	
-	oracleList = []
-	
 	#Test values in two's complement range
 	for testInts in range(0, 1000):
 		writer.write("printf(\"%i\\n\", " + str(testInts) + ");\n")
-		oracleList.append(str(testInts))
-
 	
 	#Min&Max values
 	writer.write("printf(\"%i\\n\", " + str((2**31)-1) + ");\n")
 	writer.write("printf(\"%i\\n\", " + str(-(2**31)) + ");\n")
-	oracleList.append(str((2**31)-1))
-	oracleList.append(str(-(2**31)))
 	
 	#Finish C file
 	writer.write("exit(0);}\r\n")
@@ -40,13 +36,13 @@ def testIntegers():
 	#Compile and execute
 	subprocess.call(["musl/bin/musl-gcc", "-static", "testIntegers.c", "-w", "-o", "testIntegers"])
 	proc = subprocess.Popen("./testIntegers", shell=True, stdout=subprocess.PIPE)
+	fuzFile = open('testIntegers.txt', 'w')
 	
-	#Print non-empty output
-	out, err = proc.communicate()
-	listOut = filter(None, out.split('\n'))
-	if(listOut != oracleList) :
-		print("Error in strings returned from testIntegers.c")
-	print("-----Finished executing testIntegers.c-----")
+	for line in proc.stdout:
+		fuzFile.write(line)
+
+	fuzFile.close()
+	print("-----Finished executing testIntegers.c-----\n")
 	
 def testChars():
 	print("-----Generating testChars.c-----")
@@ -58,13 +54,10 @@ def testChars():
 	writer.write("int main (int argc, const char *argv[])\r\n")
 	writer.write("{\r\n")
 	
-	oracleList = []
-	
 	#Test values in two's complement range
 	for testChars in range(32, 127):
 		if(testChars != 39) and (testChars != 92) :
 			writer.write("printf(\"%c\\n\", \'" + str(unichr(testChars)) + "\');\n")
-			oracleList.append(str(unichr(testChars)))
 	
 	#Finish C file
 	writer.write("exit(0);}\r\n")
@@ -76,28 +69,139 @@ def testChars():
 	subprocess.call(["musl/bin/musl-gcc", "-static", "testChars.c", "-w", "-o", "testChars"])
 	proc = subprocess.Popen("./testChars", shell=True, stdout=subprocess.PIPE)
 	
-	#Print non-empty output
-	out, err = proc.communicate()
-	listOut = filter(None, out.split('\n'))
-	if(listOut != oracleList) :
-		print("Error in strings returned from testChars.c")
-	print("-----Finished executing testChars.c-----")
+	fuzFile = open('testChars.txt', 'w')
 	
+	for line in proc.stdout:
+		fuzFile.write(line)
+		
+	fuzFile.close()
+	print("-----Finished executing testChars.c-----\n")
+	
+#data type range: 3.4E +/- 38
 def testFloats():
-	print("Todo")
+	print("-----Generating testFloats.c-----")
+	#Header of C file
+	writer = open("testFloats.c", "w")
+	writer.write("#include <stdio.h>\r\n")
+	writer.write("#include <stdlib.h>\r\n")
+	writer.write("\r\n")
+	writer.write("int main (int argc, const char *argv[])\r\n")
+	writer.write("{\r\n")
+	
+	#Test values in 32 bit range
+	for testFloats in range(0, 1000):
+		dec = decimal.Decimal(random.randrange(10000))/100
+		writer.write("printf(\"%f\\n\", " + str(dec) + ");\n")
+	
+	#Min&Max values
+	writer.write("printf(\"%f\\n\", " + str((3.4**38)) + ");\n")
+	writer.write("printf(\"%f\\n\", " + str((3.4**(-38))) + ");\n")
+	
+	#Finish C file
+	writer.write("exit(0);}\r\n")
+	writer.close()
+	
+	print("Compiling and executing testFloats.c")
+	
+	#Compile and execute
+	subprocess.call(["musl/bin/musl-gcc", "-static", "testFloats.c", "-w", "-o", "testFloats"])
+	proc = subprocess.Popen("./testFloats", shell=True, stdout=subprocess.PIPE)
+	
+	fuzFile = open('testFloats.txt', 'w')
+	
+	for line in proc.stdout:
+		fuzFile.write(line)
+		
+	fuzFile.close()
+	print("-----Finished executing testFloats.c-----\n")
 	
 def testStrings():
-	print("Todo")
+	print("-----Generating testStrings.c-----")
+	#Header of C file
+	writer = open("testStrings.c", "w")
+	writer.write("#include <stdio.h>\r\n")
+	writer.write("#include <stdlib.h>\r\n")
+	writer.write("\r\n")
+	writer.write("int main (int argc, const char *argv[])\r\n")
+	writer.write("{\r\n")
 	
-def testCombinations():
-	print("Todo")
+	#Test values with size = 10
+	for testInts in range(0, 1000):
+		testString = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(10))
+		writer.write("printf(\"%s\\n\", \"" + str(testString) + "\");\n")
+
+	
+	#Finish C file
+	writer.write("exit(0);}\r\n")
+	writer.close()
+	
+	print("Compiling and executing testStrings.c")
+	
+	#Compile and execute
+	subprocess.call(["musl/bin/musl-gcc", "-static", "testStrings.c", "-w", "-o", "testStrings"])
+	proc = subprocess.Popen("./testStrings", shell=True, stdout=subprocess.PIPE)
+	
+	fuzFile = open('testStrings.txt', 'w')
+	
+	for line in proc.stdout:
+		fuzFile.write(line)
+		
+	fuzFile.close()
+	print("-----Finished executing testStrings.c-----\n")
+	
+def runAllWithGCC():
+	print("-----Compiling and executing all .c files with GCC-----")
+	#testInts
+	subprocess.call(["gcc", "testIntegers.c", "-w", "-o", "gccIntegers"])
+	proc = subprocess.Popen("./gccIntegers", shell=True, stdout=subprocess.PIPE)
+	fuzFile = open('gccIntegers.txt', 'w')
+	for line in proc.stdout:
+		fuzFile.write(line)
+		
+	#testChars
+	subprocess.call(["gcc", "testChars.c", "-w", "-o", "gccChars"])
+	proc = subprocess.Popen("./gccChars", shell=True, stdout=subprocess.PIPE)
+	fuzFile = open('gccChars.txt', 'w')
+	for line in proc.stdout:
+		fuzFile.write(line)
+		
+	#testFloats
+	subprocess.call(["gcc", "testFloats.c", "-w", "-o", "gccFloats"])
+	proc = subprocess.Popen("./gccFloats", shell=True, stdout=subprocess.PIPE)
+	fuzFile = open('gccFloats.txt', 'w')
+	for line in proc.stdout:
+		fuzFile.write(line)
+		
+	#testStrings
+	subprocess.call(["gcc", "testStrings.c", "-w", "-o", "gccStrings"])
+	proc = subprocess.Popen("./gccStrings", shell=True, stdout=subprocess.PIPE)
+	fuzFile = open('gccStrings.txt', 'w')
+	for line in proc.stdout:
+		fuzFile.write(line)
+	print("-----Finished executing GCC compiled files-----\n")
+	
+def diffFiles():
+	cmpFiles("testIntegers.txt", "gccIntegers.txt")
+	cmpFiles("testChars.txt", "gccChars.txt")
+	cmpFiles("testFloats.txt", "gccFloats.txt")
+	cmpFiles("testStrings.txt", "gccStrings.txt")
+	
+def cmpFiles(file1, file2):
+	print("-----Diff'ing "+file1+" with "+file2+"-----")
+	output = ""
+	output = output + subprocess.check_output(["diff", file1, file2])
+	if(output != ""):
+		print(file1+" and "+file2+" have differences!")
+		print(output)
+	print("-----Finished diff'ing-----\n")
 	
 def main():
 	testIntegers()
 	testChars()
 	testFloats()
 	testStrings()
-	testCombinations()
+	runAllWithGCC()
+	diffFiles()
 
 if __name__ == '__main__':
 	main()
