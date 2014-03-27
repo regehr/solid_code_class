382812362d9048f7d1e678b3829c0ecbdae1cb1e
Daniel Setser <danielsetser@gmail.com>
2014-03-27 12:22:32 -0600

    Added more to fuzzer

--- fuzzer/dsetser/fuzzer.py
+++ fuzzer/dsetser/fuzzer.py
@@ -4,30 +4,46 @@ import filecmp
 import sys
 import random
 
+formatStrings = ["%d", "%ll", "%f", "%x", "a"]
+conversions = ["atoi", "atoll", "atof", "atol", "atof"]
+randoms = [lambda: random.randint(-2**31, (2**31)-1), \
+           lambda: random.randint(-2**63, (2**63)-1), \
+           lambda: random.uniform(-2**63, (2**63)-1), \
+           lambda: random.randint(-2**31, (2**31)-1), \
+           lambda: random.uniform(-2**63, (2**63)-1)]
+
+def generateCFile():
+  cProgram = '#include <stdio.h>\n#include <stdlib.h>\n#include <string.h>\n\nint main(int argc, char* argv[]) {\n'
+     
+  for i in range(0, len(formatStrings)):
+    cProgram += '\tif(strcmp(argv[1], "' + formatStrings[i] + '") == 0)\n'
+    cProgram += '\t\tprintf(argv[1], ' + conversions[i] + '(argv[2])); \n'
+  cProgram += '\treturn 0;\n}'
+  return cProgram;
+
 def init():
   testFile = open('test.c', 'w')
-  testFile.write('#include <stdio.h>\nint main(int argc, char* argv[]) { printf(argv[1], atoi(argv[2])); }')
+  testFile.write(generateCFile())
   testFile.close()
   subprocess.call(["musl-gcc", "-static", "-o", "muslOut", "test.c"])
   subprocess.call(["gcc", "-o", "gccOut", "test.c"])
 
 def testEquality(formatString, input):
-  subprocess.call(["./muslOut", formatString, input], stdout=open("musl.txt", "w"))
-  subprocess.call(["./gccOut", formatString, input], stdout=open("gcc.txt", "w"))
-  return filecmp.cmp("musl.txt", "gcc.txt")
+  musl = subprocess.check_output(["./muslOut", formatString, input])
+  gcc = subprocess.check_output(["./gccOut", formatString, input])
+  return musl == gcc
 
 def clean():
   os.remove('test.c')
   os.remove('muslOut')
   os.remove('gccOut')
-  os.remove('musl.txt')
-  os.remove('gcc.txt')
 
 def fuzz():
-  for i in range(0, 1000):
-    testing = random.randint((-2**32)-1, 2**32)
-    if(not testEquality("%d", str(testing))):
-      print(str(testing) + " is Incorrect")
+  for i in range(0, len(formatStrings)):
+    for count in range(0, 1000):
+      testing = randoms[i]()
+      if(not testEquality(formatStrings[i], str(testing))):
+        print(str(testing) + " is Incorrect")
 
 def main():
   init()
