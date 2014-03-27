9fb89d808dfd460f2cce3d26f05f052848b8ee62
Brenden Tyler <brendenjtyler@gmail.com>
2014-03-27 12:21:39 -0600

    Updated fuzzer for better testing of char case

--- fuzzer/brendenjtyler/test.py
+++ fuzzer/brendenjtyler/test.py
@@ -2,24 +2,44 @@ import subprocess
 import sys
 from random import randint
 
-#open up a file to write to
-writeTo = open("hello.c", "w")
+def printHeaders():
+    #open up a file to write to
+    writeTo = open("hello.c", "w")
 
-#write the headers for the c program
-writeTo.write("#include <stdio.h>\r\n")
-writeTo.write("\r\n")
-writeTo.write("int main (int argc, char **argv)\r\n")
-writeTo.write("{\r\n")
+    #write the headers for the c program
+    writeTo.write("#include <stdio.h>\n")
+    writeTo.write("\n")
+    writeTo.write("int main (int argc, char **argv)\n")
+    writeTo.write("{\n")
 
-#write the printf lines (right now we're doing the highest 1000 ints and trying to print them as chars)
-for printLines in range(2**32-1024, 2**32):
-    writeTo.write("printf(\"%c\\n\", " + str(printLines) + ");\n")
+    return writeTo
+    
+def printClose(writeTo):
+    #close up c program
+    writeTo.write("}\n")
 
-#close up c program
-writeTo.write("}\r\n")
+    writeTo.close()
 
-#close the file
-writeTo.close()
+#########################################################
+#                                                       #
+#                CHARACTER TEST                         #
+#                                                       #
+#########################################################
+
+writeTo = printHeaders()
+
+#print out the character test for loop
+writeTo.write("int i;\n")
+writeTo.write("for(i = 0; i < 256; i++)\n")
+writeTo.write("{\n")
+writeTo.write("printf(\"%c\\n\", (char)i);")
+writeTo.write("}\n")
+
+printClose(writeTo)
+
+result = []
+for char in range(0, 256):
+    result.append(chr(char))
 
 #compile it (with warning turned off because the compiler complains about int and chars and such)
 subprocess.call(["/usr/local/musl/bin/musl-gcc", "hello.c", "-w", "-o", "hello"])
@@ -33,6 +53,46 @@ out, err = p.communicate()
 #turn the output into a list (removing empty entries)
 listOut = filter(None, out.split('\n'))
 
-#print out the list
-print(listOut)
+for index in range(0, 256):
+    if(ord(result[index]) != ord(listOut[index])):
+        print("Error at " + str(index) + "\n")
+        print("Python char is: " + str(ord(result[index])) + "\n")
+        print("C char is: " + str(ord(listOut[index])) + "\n")
+        print(result)
+        print("")
+        print(listOut)
+        break
 
+#########################################################
+#                                                       #
+#                     FLOAT TEST                        #
+#                                                       #
+#########################################################
+
+
+writeTo = printHeaders()
+
+#print out the character test for loop
+writeTo.write("int i;\n")
+writeTo.write("for(i = 0; i < 256; i++)\n")
+writeTo.write("{\n")
+writeTo.write("printf(\"%c\\n\", (char)i);")
+writeTo.write("}\n")
+
+printClose(writeTo)
+
+result = []
+for char in range(0, 256):
+    result.append(chr(char))
+
+#compile it (with warning turned off because the compiler complains about int and chars and such)
+subprocess.call(["/usr/local/musl/bin/musl-gcc", "hello.c", "-w", "-o", "hello"])
+
+#run it
+p = subprocess.Popen(["./hello"], stdout=subprocess.PIPE)
+
+#get the output
+out, err = p.communicate()
+
+#turn the output into a list (removing empty entries)
+listOut = filter(None, out.split('\n'))
