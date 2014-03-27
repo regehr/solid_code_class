a302c1b8c76e2e35b155989891a33b0b40902e80
John Regehr <regehr@cs.utah.edu>
2014-03-26 22:17:19 -0600

    new standalone version of musl printf, suitable for coverage monitoring

---
+++ fuzzer/musl-printf-standalone/test-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "musl.h"
+
+#define LEN 10000
+char buf[LEN];
+
+int main (void)
+{
+
+  // THIS IS JOHN'S TEST CODE, DELETE IT AND PUT YOUR CODE HERE
+  musl_snprintf (buf, LEN, "hello %d\n", 10);
+  printf ("%s", buf);
+
+  return 0;
+}
