0e528cc4ff228e3ad2e192c4620fcd649b3e607b
Taylor J Stapleton <tstaplet@lab1-20.eng.utah.edu>
2014-03-30 19:26:20 -0600

    ~72% coverage, 64 test cases

--- fuzzer/taylorstapleton/test.py
+++ fuzzer/taylorstapleton/test.py
@@ -1,6 +1,7 @@
 from subprocess import CalledProcessError, check_output
 import os
 import random
+import sys
 
 
 def newFile(fileName):
@@ -19,12 +20,12 @@ def writeMain(file):
         file.write("#include <stdio.h>\n")
         file.write("int main(void){\n")
 
-def writePrintMusl(file, printType, printValue):
-	file.write("musl_snprintf (buf, LEN, \"%" + printType + "\" , " + str(printValue) + ");\n");
+def writePrintMusl(file, printType, printValue, printOpt):
+	file.write("musl_snprintf (buf, LEN, \"%" + printOpt + printType + "\" , " + str(printValue) + ");\n");
 	file.write("printf(\"%s\", buf);")
 
-def writePrint(file, printType, printValue):
-        file.write("printf (\"%" + printType + "\" , " + str(printValue) + ");\n");
+def writePrint(file, printType, printValue, printOpt):
+        file.write("printf (\"%" + printOpt + printType + "\" , " + str(printValue) + ");\n");
 
 def writeClose(file):
 	file.write("return 0;\n}")
@@ -53,9 +54,12 @@ def compileFiles(fileList):
 
 def compileFileGcc(fileName):
 	try:
-		check_output(["gcc" , fileName , "-o", fileName[:-2] + "gcc", "-I " + os.getcwd()])# "fwrite.c" , "snprintf.c" , "vfprintf.c" , "vsnprintf.c" , "-o" , fileName[:-2] + "gcc"])
+		if(check_output(["gcc" , fileName , "-o", fileName[:-2] + "gcc" , "fwrite.o" , "snprintf.o" , "vfprintf.o" , "vsnprintf.o" , "-o" , fileName[:-2] + "gcc", "-lgcov"]) != ""):
+			sys.exit("")
+		
 	except CalledProcessError as e:
 		print e
+		sys.exit("")
 
 def runFiles(fileList, testCase , logfile):
 
@@ -72,9 +76,11 @@ def runFiles(fileList, testCase , logfile):
 
 		if(output != outputGcc):
 			logfile.write("print failed! expected = " + output + " actual = " + outputGcc + "file = \n" + file + "\n")
-			logfile.write("printf params = " + str(testCase[0]) + " " + str(testCase[1]) + "\n" + "\n")
+			logfile.write("printf params = " + str(testCase[0]) + " " + str(testCase[1]) + " " + str(testCase[2]) + "\n" + "\n")
+			return False
 		else:
-			logfile.write("passed " + str(testCase[0]) + " " + str(testCase[1]) + "\n")
+			logfile.write("passed " + str(testCase[0]) + " " + str(testCase[1]) + " " + str(testCase[2]) + " " + " musl output = " + outputGcc + " reg output = " + output + "\n")
+	return True
 
 
 def removeFiles(fileList):
@@ -82,8 +88,8 @@ def removeFiles(fileList):
 		check_output(["rm" , file , file[:-2] , file[:-2]+"gcc"])
 
 def createTestCase():
-	randomNumber = random.randint(0,8)
-	testCase = ["",""]
+	randomNumber = random.randint(0,61)
+	testCase = ["","",""]
 	
 	if(randomNumber == 0):
 		testCase[0] = "d"
@@ -110,10 +116,184 @@ def createTestCase():
                 testCase[0] = "F"
 		testCase[1] = str(random.getrandbits(15)) + "." + str(random.getrandbits(15))
 	elif(randomNumber == 8):
+                testCase[0] = "dhh"
+                testCase[1] = str(random.getrandbits(8))
+	elif(randomNumber == 9):
                 testCase[0] = "u"
                 testCase[1] = str(random.getrandbits(31))
+	elif(randomNumber == 10):
+                testCase[0] = "dh"
+                testCase[1] = str(random.getrandbits(16))
+	elif(randomNumber == 11):
+                testCase[0] = "dl"
+                testCase[1] = str(random.getrandbits(32))
+        elif(randomNumber == 12):
+                testCase[0] = "dll"
+                testCase[1] = str(random.getrandbits(63))
+        elif(randomNumber == 13):
+                testCase[0] = "ihh"
+                testCase[1] = str(random.getrandbits(8))
+        elif(randomNumber == 14):
+                testCase[0] = "ih"
+                testCase[1] = str(random.getrandbits(16))
+        elif(randomNumber == 15):
+                testCase[0] = "il"
+                testCase[1] = str(random.getrandbits(32))
+        elif(randomNumber == 16):
+                testCase[0] = "ill"
+                testCase[1] = str(random.getrandbits(63))
+	elif(randomNumber == 17):
+                testCase[0] = "fl"
+                testCase[1] = str(random.getrandbits(30)) + "." + str(random.getrandbits(30))
+        elif(randomNumber == 18):
+                testCase[0] = "fL"
+                testCase[1] = str(random.getrandbits(30)) + "." + str(random.getrandbits(30))
+        elif(randomNumber == 19):
+                testCase[0] = "F"
+                testCase[1] = str(random.getrandbits(15)) + "." + str(random.getrandbits(15))
+        elif(randomNumber == 20):
+                testCase[0] = "Fl"
+                testCase[1] = str(random.getrandbits(30)) + "." + str(random.getrandbits(30))
+        elif(randomNumber == 21):
+                testCase[0] = "FL"
+                testCase[1] = str(random.getrandbits(30)) + "." + str(random.getrandbits(30))
+        elif(randomNumber == 22):
+                testCase[0] = "c"
+                testCase[1] = str(random.getrandbits(8))
+        elif(randomNumber == 23):
+                testCase[0] = "xhh"
+                testCase[1] = str(random.getrandbits(8))
+        elif(randomNumber == 24):
+                testCase[0] = "xh"
+                testCase[1] = str(random.getrandbits(16))
+        elif(randomNumber == 25):
+                testCase[0] = "xl"
+                testCase[1] = str(random.getrandbits(32))
+        elif(randomNumber == 26):
+                testCase[0] = "xll"
+                testCase[1] = str(random.getrandbits(63))
+        elif(randomNumber == 27):
+                testCase[0] = "u"
+                testCase[1] = str(random.getrandbits(32))
+        elif(randomNumber == 28):
+                testCase[0] = "uh"
+                testCase[1] = str(random.getrandbits(8))
+        elif(randomNumber == 29):
+                testCase[0] = "uhh"
+                testCase[1] = str(random.getrandbits(16))
+        elif(randomNumber == 30):
+                testCase[0] = "ul"
+                testCase[1] = str(random.getrandbits(32))
+        elif(randomNumber == 31):
+                testCase[0] = "ull"
+                testCase[1] = str(random.getrandbits(63))
+        elif(randomNumber == 32):
+                testCase[0] = "e"
+                testCase[1] = str(random.getrandbits(15)) + "." + str(random.getrandbits(15))
+        elif(randomNumber == 33):
+                testCase[0] = "E"
+                testCase[1] = str(random.getrandbits(15)) + "." + str(random.getrandbits(15))
+        elif(randomNumber == 34):
+                testCase[0] = "a"
+                testCase[1] = str(random.getrandbits(15)) + "." + str(random.getrandbits(15))
+        elif(randomNumber == 35):
+                testCase[0] = "A"
+                testCase[1] = str(random.getrandbits(15)) + "." + str(random.getrandbits(15))
+        elif(randomNumber == 36):
+                testCase[0] = "el"
+                testCase[1] = str(random.getrandbits(15)) + "." + str(random.getrandbits(15))
+        elif(randomNumber == 37):
+                testCase[0] = "eL"
+                testCase[1] = str(random.getrandbits(30)) + "." + str(random.getrandbits(30))
+        elif(randomNumber == 38):
+                testCase[0] = "al"
+                testCase[1] = str(random.getrandbits(15)) + "." + str(random.getrandbits(15))
+        elif(randomNumber == 39):
+                testCase[0] = "aL"
+                testCase[1] = str(random.getrandbits(30)) + "." + str(random.getrandbits(30))
+        elif(randomNumber == 40):
+                testCase[0] = "El"
+                testCase[1] = str(random.getrandbits(15)) + "." + str(random.getrandbits(15))
+        elif(randomNumber == 41):
+                testCase[0] = "EL"
+                testCase[1] = str(random.getrandbits(30)) + "." + str(random.getrandbits(30))
+        elif(randomNumber == 42):
+                testCase[0] = "Al"
+                testCase[1] = str(random.getrandbits(15)) + "." + str(random.getrandbits(15))
+        elif(randomNumber == 43):
+                testCase[0] = "AL"
+                testCase[1] = str(random.getrandbits(30)) + "." + str(random.getrandbits(30))
+        elif(randomNumber == 44):
+                testCase[0] = "g"
+                testCase[1] = str(random.getrandbits(15)) + "." + str(random.getrandbits(15))
+        elif(randomNumber == 45):
+                testCase[0] = "gl"
+                testCase[1] = str(random.getrandbits(15)) + "." + str(random.getrandbits(15))
+        elif(randomNumber == 46):
+                testCase[0] = "gL"
+                testCase[1] = str(random.getrandbits(30)) + "." + str(random.getrandbits(30))
+        elif(randomNumber == 47):
+                testCase[0] = "G"
+                testCase[1] = str(random.getrandbits(15)) + "." + str(random.getrandbits(15))
+        elif(randomNumber == 48):
+                testCase[0] = "Gl"
+                testCase[1] = str(random.getrandbits(15)) + "." + str(random.getrandbits(15))
+        elif(randomNumber == 49):
+                testCase[0] = "GL"
+                testCase[1] = str(random.getrandbits(30)) + "." + str(random.getrandbits(30))
+        elif(randomNumber == 50):
+                testCase[0] = "ohh"
+                testCase[1] = str(random.getrandbits(8))
+        elif(randomNumber == 51):
+                testCase[0] = "oh"
+                testCase[1] = str(random.getrandbits(16))
+        elif(randomNumber == 52):
+                testCase[0] = "ol"
+                testCase[1] = str(random.getrandbits(32))
+        elif(randomNumber == 53):
+                testCase[0] = "oll"
+                testCase[1] = str(random.getrandbits(63))
+        elif(randomNumber == 54):
+                testCase[0] = "xhh"
+                testCase[1] = str(random.getrandbits(8))
+        elif(randomNumber == 55):
+                testCase[0] = "xh"
+                testCase[1] = str(random.getrandbits(16))
+        elif(randomNumber == 56):
+                testCase[0] = "xl"
+                testCase[1] = str(random.getrandbits(32))
+        elif(randomNumber == 57):
+                testCase[0] = "xll"
+                testCase[1] = str(random.getrandbits(63))
+        elif(randomNumber == 58):
+                testCase[0] = "Xhh"
+                testCase[1] = str(random.getrandbits(8))
+        elif(randomNumber == 59):
+                testCase[0] = "Xh"
+                testCase[1] = str(random.getrandbits(16))
+        elif(randomNumber == 60):
+                testCase[0] = "Xl"
+                testCase[1] = str(random.getrandbits(32))
+        elif(randomNumber == 61):
+                testCase[0] = "Xll"
+                testCase[1] = str(random.getrandbits(63))
 
 
+	randomOper = random.randint(0,1)
+	if(randomOper == 1):
+		testCase[2] = testCase[2] + "-"
+
+        randomOper = random.randint(0,1)
+        if(randomOper == 1):
+                testCase[2] = testCase[2] + "+"
+
+        randomOper = random.randint(0,1)
+        if(randomOper == 1):
+                testCase[2] = testCase[2] + " "
+
+        randomOper = random.randint(0,1)
+        if(randomOper == 1):
+                testCase[2] = testCase[2] + "#"
 
 
 	return testCase	
@@ -121,9 +301,9 @@ def createTestCase():
 def main():
 	logFile = newFile("logfile")
 	n = 0
+	failedTests = 0
 	while(True):
-		n = n + 1	
-		print n
+		n = n + 1
 		fileNameMusl = os.getcwd() + "/" + "testFileMusl"
 		fileName = os.getcwd() + "/" + "testFile"
 
@@ -138,8 +318,8 @@ def main():
 		writeMain(file)
 
 		#write the body
-		writePrintMusl(fileMusl, testCase[0], testCase[1])
-		writePrint(file, testCase[0], testCase[1])
+		writePrintMusl(fileMusl, testCase[0], testCase[1], testCase[2])
+		writePrint(file, testCase[0], testCase[1], testCase[2])
 
 		#close the files
 		writeClose(fileMusl)
@@ -147,9 +327,17 @@ def main():
 
 		compileFiles([fileNameMusl + ".c", fileName + ".c"])
 	
-		runFiles([fileName] , testCase , logFile)
+		if(runFiles([fileName] , testCase , logFile) != True):
+			print "failed test " + str(n)
+			failedTests= failedTests + 1
+		else:
+			print "passed test " + str(n)
 	
 		#removeFiles(fileList)
+		if(n % 100 == 0):
+			print "coverage results so far:\n"
+			print os.system("gcov vfprintf.c")
+			print "failed tests = " + str(failedTests)
 
 if __name__ == "__main__":
 	main()
