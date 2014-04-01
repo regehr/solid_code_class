09129715db5b60bd294c9a8b9a9a89342fe42432
lgao <gao_lynn@hotmail.com>
2014-03-31 20:56:15 -0600

    adding more to fuzzer

--- fuzzer/lynntesia/fuzzer.py
+++ fuzzer/lynntesia/fuzzer.py
@@ -7,7 +7,7 @@ import filecmp
 import decimal
 
 
-def test(value):
+def fuzz_it(value):
 	file = open("test-printf.c", "w")
 	file.write("#include <stdio.h>\n")
 	file.write("#include <stdarg.h>\n")
@@ -20,10 +20,49 @@ def test(value):
 	file.write("return 0;\n}")
 	file.close()
 
+def fuzz_it_type(value, typeof):
+	file = open("test-printf.c", "w")
+	file.write("#include <stdio.h>\n")
+	file.write("#include <stdarg.h>\n")
+	file.write("#include \"musl.h\"\n")
+	file.write("#define LEN 10000\n")
+	file.write("char buf[LEN];\n")
+	file.write("int main(int argc, const char *argv[]) {\n")
+	file.write("musl_snprintf(buf, LEN, \"%" +  typeof +  "\" , " + str(value) + ");\n")
+	file.write("printf(\"%s\", buf);")
+	file.write("return 0;\n}")
+	file.close()
+
+def string_fuzz():
+	n = 100000
+	test_words = large_string(n)
+	ret =  " \""+ test_words+ " \\n \" "
+	return ret
+
+def large_string(n):
+	ret = ""
+	for i in xrange(n):
+		ret += random.choice(string.letters)
+	return ret
+
+def int_fuzz():
+	n = 60
+	test_ints = random_little_int(n)
+	return test_ints
+	
+
+def random_little_int(n):
+	return random.getrandbits(n)
 
 def main():
-	s = " \" Hello fuzzer \\n \" "
-	test(s)
+	hello_test = " \" Hello fuzzer \\n \" "
+	fuzz_it(hello_test)
+	string_test = string_fuzz()
+	fuzz_it(string_test)
+	int_test = int_fuzz()
+	fuzz_it_type(int_test, 'i')
+	
+	
 	print os.system("gcov vfprintf.c")
 
 if __name__ == "__main__":
