6cfa3e7f19b0d642bc01ccf435d14607ce882137
lgao <gao_lynn@hotmail.com>
2014-03-31 20:56:51 -0600

    removed out file

--- fuzzer/lynntesia/test-printf.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdarg.h>
-#include "musl.h"
-#define LEN 10000
-char buf[LEN];
-int main(int argc, const char *argv[]) {
-musl_snprintf(buf, LEN, "%s" ,  " Hello world \n " );
-printf("%s", buf);return 0;
-}
