b0493314fb1f5d8139f746eb6e4f121a68a3d618
Todd Johnson <tojohnso@lab2-13.eng.utah.edu>
2014-03-31 19:55:20 -0600

    Refactored code to run with code coverage Merge branch 'master' of https://github.com/regehr/solid_code_class
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- fuzzer/toddjohnson/fuzzer.py
+++ fuzzer/toddjohnson/fuzzer.py
@@ -6,109 +6,24 @@ import string
 import filecmp
 import decimal
 
-#Initial Hello World Test Case
-def helloTest():
-    output = "Hello World"
-    with open("hello.c", "w") as file:
-        file.write("#include<stdio.h>\nint main(){return printf(\"" + output + "\");}")
-        file.close()
-    subprocess.call(["gcc", "hello.c", "-o", "hello"])
-    subprocess.call(["/home/tojohnso/musl-1.0.0/musl/bin/musl-gcc", "-static", "hello.c", "-o", "muslHello"])
-    with open('hello.txt', 'w') as t:
-        test1 = subprocess.call(["./hello"], stdout=t)
-    with open('muslHello.txt', 'w') as m:
-        test2 = subprocess.call(["./muslHello"], stdout=m)
-
-    if test1 != test2 or filecmp.cmp('hello.txt', 'muslHello.txt') == False:
-        print "Test Failed"
-        exit()
-
-    os.remove("hello.c")
-    os.remove("hello")
-    os.remove("muslHello")
-
-    print "Hello World  Test Passed"
-
-#Generate a large random string
-def randomString(size = 4096, ranString = string.ascii_uppercase + string.digits):
-    return ''.join(random.choice(ranString) for n in range(size))
-
-#Int test pulled from another fuzzer
-def intTest():
-    for i in xrange(-5, 5):
-        with open("test.c", "w") as file:
-            file.write("#include<stdio.h>\nint main(){return printf(\"" + "%d" + "\"," + str(i) + ");}")
-            file.close()
-
-        subprocess.call(["gcc", "test.c", "-o", "test"])
-        subprocess.call(["/home/tojohnso/musl-1.0.0/musl/bin/musl-gcc", "-static", "test.c", "-o", "muslTest"])
-
-        with open('test.txt', 'w') as t:
-            test1 = subprocess.call(["./test"], stdout=t)
-        with open('musl.txt', 'w') as m:
-            test2 = subprocess.call(["./muslTest"], stdout=m)
-
-        if test1 != test2 or filecmp.cmp('test.txt', 'musl.txt') == False:
-            print "Test Failed"
-            exit()
-
-
-        os.remove("test.c")
-        os.remove("test")
-        os.remove("muslTest")
-
-    print "Int Test Passed"
-
-#Random String Test to see if printf can handle a large random string
-def stringTest():
-    output = randomString()
-    with open("tester.c", "w") as file:
-        file.write("#include<stdio.h>\nint main(){return printf(\"" + output + "\");}")
-        file.close()
-    subprocess.call(["gcc", "tester.c", "-o", "tester"])
-    subprocess.call(["/home/tojohnso/musl-1.0.0/musl/bin/musl-gcc", "-static", "tester.c", "-o", "muslTester"])
-    with open('tester.txt', 'w') as t:
-        test1 = subprocess.call(["./tester"], stdout=t)
-    with open('mucltester.txt', 'w') as mt:
-        test2 = subprocess.call(["./muslTester"], stdout=mt)
-
-    os.remove("tester.c")
-    os.remove("tester")
-    os.remove("muslTester")
-
-    print "Random String Test Passed"
-
-#Special Character Test to see if printf can handle special chars
-def charTest():
-  
-    output = "!#$%%&()*+,-./:;<=>?@[\]^_{|}"
-
-    with open("test4.c", "w") as file:
-        file.write("#include<stdio.h>\nint main(){return printf(\"" + output + "\");}")
-        file.close()
-    subprocess.call(["gcc", "test4.c", "-o", "chartest"])
-    subprocess.call(["/home/tojohnso/musl-1.0.0/musl/bin/musl-gcc", "-static", "test4.c", "-o", "muslChar"])
-    with open('test4.txt', 'w') as t:
-        test1 = subprocess.call(["./chartest"], stdout=t)
-    with open('muslChar.txt', 'w') as m:
-        test2 = subprocess.call(["./muslChar"], stdout=m)
-
-    if test1 != test2 or filecmp.cmp('test4.txt', 'muslChar.txt') == False:
-        print "Test Failed"
-        exit()
-  
-
-    os.remove("test4.c")
-    os.remove("chartest")
-    os.remove("muslChar")
-
-    print "Special Char Test Passed"
+def helloTest(value):
+    file = open('test-printf.c', 'w')
+    file.write("#include <stdio.h>\n")
+    file.write("#include <stdarg.h>\n")
+    file.write("#include \"musl.h\"\n")
+    file.write("#define LEN 10000\n")
+    file.write("char buf[LEN];\n")
+    file.write("int main(void){\n")
+    file.write("musl_snprintf (buf, LEN, \"%s"  "\" , " + str(value) + ");\n");
+    file.write("printf(\"%s\", buf);")
+    file.write("return 0;\n}")
+    file.close()
 
 
 if __name__ == "__main__":
-    helloTest()
-    intTest()
-    stringTest()
-    charTest()   
+    s = "  \" Hello World \\n \" "
+    helloTest(s)
+
+    print os.system("gcov vfprintf.c")
     
 
