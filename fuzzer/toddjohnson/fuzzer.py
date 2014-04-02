8c5ad46c0f8c01844aece18ae56dde4f3063146a
Todd Johnson <tojohnso@lab1-3.eng.utah.edu>
2014-04-02 10:51:56 -0600

    Refactored fuzzer. Now getting 66% coverage.

--- fuzzer/toddjohnson/fuzzer.py
+++ fuzzer/toddjohnson/fuzzer.py
@@ -7,217 +7,48 @@ import filecmp
 import decimal
 from random import randrange
 
-#Collaborated with Lynn Gao
-
-#Helo World Test 
-#Ensures code coverage is working
-def helloTest(value):
-    file = open('test-printf.c', 'w')
-    file.write("#include <stdio.h>\n")
-    file.write("#include <stdarg.h>\n")
-    file.write("#include \"musl.h\"\n")
-    file.write("#define LEN 10000\n")
-    file.write("char buf[LEN];\n")
-    file.write("int main(void){\n")
-    file.write("musl_snprintf (buf, LEN, \"%s"  "\" , " + str(value) + ");\n");
-    file.write("printf(\"%s\", buf);")
-    file.write("return 0;\n}")
-    file.close()
-
-#Fuzz Test 
-def fuzz(value, typeof):
-    file = open("test-printf.c", "w")
-    file.write("#include <stdio.h>\n")
-    file.write("#include <stdarg.h>\n")
-    file.write("#include \"musl.h\"\n")
-    file.write("#define LEN 10000\n")
-    file.write("char buf[LEN];\n")
-    file.write("int main(int argc, const char *argv[]) {\n")
-    file.write("musl_snprintf(buf, LEN, \"%" + typeof + "\" , " + str(value) + ");\n")
-    file.write("printf(\"%s\", buf);")
-    file.write("return 0;\n}")
-    file.close()
-
-#Fuzz Test adding options -, +, " ", #
-def fuzz_options(value, typeof, option):
-    file = open("test-printf.c", "w")
-    file.write("#include <stdio.h>\n")
-    file.write("#include <stdarg.h>\n")
-    file.write("#include \"musl.h\"\n")
-    file.write("#define LEN 10000\n")
-    file.write("char buf[LEN];\n")
-    file.write("int main(int argc, const char *argv[]) {\n")
-    file.write("musl_snprintf(buf, LEN, \"%" + option + typeof + "\" , " + str(value) + ");\n")
-    file.write("printf(\"%s\", buf);")
-    file.write("return 0;\n}")
-    file.close()
-
-#Test as many types (i.e. %d, %s, %i, etc.) as can think of
-
-#Int Test
-def intTest():
-    fuzz(-2147483648, "i")
-    fuzz_options(-2147483648, "i", "-")
-    fuzz_options(-2147483648, "i", "+")
-    fuzz_options(-2147483648, "i", "\" \" ")
-    fuzz_options(-2147483648, "i", "#")
-
-def uintTest():
-    fuzz(4294967295, "u")
-    fuzz(4294967295, "o")
-    fuzz_options(4294967295, "o", "#")
-    fuzz_options(4294967295, "u", "-")
-    fuzz_options(4294967295, "u", "+")
-    fuzz_options(4294967295, "u", " ")
-    fuzz_options(4294967295, "u", "#")
-
-#Hext Test
-def hexTest():
-    fuzz(-2147483648, "x")
-    fuzz(4294967295, "x")
-    fuzz(4294967295, "X")
-    fuzz_options(4294967295, "x", "#")
-
-#Long Test
-def longTest():
-    fuzz(-1024, "l")
-    fuzz(1024, "ul")
-
-#Long Long
-def longlongTest():
-    fuzz(-2147483647L, "lli")
-    fuzz(4294967295L, "llu")
-    fuzz(-2147483647L, "llx")
-    fuzz(4294967295L, "llx")
-
-def longdoubleTest():
-    fuzz(1e37, "Lf")
-    fuzz(1e37, "llf")
-    fuzz(1e-37, "llf")
-    fuzz(1e+37, "lle")
-    fuzz(1e-37, "lle")
-
-#Char Test
-def charTest(value):
-    fuzz(value, "c")
-    fuzz(value, "u")
-
-#Double Test
-def doubleTest():
-    fuzz(12.22, "d")
-    fuzz(12.22, "E")
-    fuzz(12.22, "g")
-
-#Float Test
-def floatTest():
-    fuzz(1.34, "f")
-    fuzz(1.34, "F")
-    fuzz(1.34, "G")
-
-#Short Test
-def ushortTest():
-    fuzz(65535, "hu")
-    fuzz(65535, "hx")
-
-def shortTest():
-    fuzz(-32768 , "hi")
-    fuzz(-32768, "hx")
-
-
-#Fuzz randomness
-def int_fuzz():
-    n = 63
-    test_ints = random_int(n)
-    return test_ints
-
-def sm_int_fuzz():
-    n = 31
-    test_ints = random_int(n)
-    return test_ints
-
+def large_string(n):
+    ret = ""
+    for i in xrange(n):
+        ret += random.choice(string.letters)
+    return ret
 
 def random_int(n):
     return random.getrandbits(n)
 
-def large_int_fuzz():
-    n = 100
-    test_ints = random_int(n)
-    return test_ints
+def fuzz1():
+    typ = "c %d %f %e %s %d %i %x %u %o %g %% %a %l %ll %p"
+    c = "'" + random.choice(string.letters) + "'"
+    d = random_int(8)
+    f = random.uniform(1.0, 1000.1)
+    e = 12345678.
+    s = '"'+large_string(10)+'"'
+    d = random_int(4)
+    i = 14
+    x = hex(random_int(16))
+    o = oct(random_int(32))
+    sym = "'%'"
+    dH = hex(12)
 
-def char_fuzz():
-    for i in range(255):
-        value  = str(randrange(255))
-        fuzz(value, "c")
-    for i in range(255):
-        value2 = str(randrange(-128, 127))
-        fuzz(value2, "c")
-    a = format(random.getrandbits(8), '08b')
-    num = int(a, 2)
-    fuzz(num, "c")
+    file = open("test-printf.c", "w")
+    file.write("#include <stdio.h>\n")
+    file.write("#include <stdarg.h>\n")
+    file.write("#include <math.h>\n")
+    file.write("#include \"musl.h\"\n")
+    file.write("#define LEN 10000\n")
+    file.write("char buf[LEN];\n")
+    file.write("int main(int argc, const char *argv[]) {\n")
+    file.write("musl_snprintf(buf, LEN, \"%" +  typ +  "\" , " + str(c) + "," + str(d) + "," +str(f) + "," +str(e) + "," +str(s) + "," +str(d) +  "," +str(i) +  "," + str(x) +  "," + str(i)+  "," +str(o) + "," +str(d) +"," +str(sym) +  "," + str(dH) +  "," + str(d) +  "," + str(d) +  "," + str(d) + ");\n")
+    file.write("printf(\"%s\", buf);")
+    file.write("return 0;\n}")
+    file.close()
 
-def u_short_int():
-    for i in range(200):
-        ui = str(randrange(0, 65535))
-        fuzz(ui, "u")
-
-def short_int():
-    for i in range(200):
-        ssi = str(randrange(-32768, 32767))
-        fuzz(ssi, "i")
-
-# short int, unsigned short int, unsigned int
-def bit_16():
-    a = format(random.getrandbits(16), '016b')
-    num = int(a, 2)
-    fuzz_options(num, "d", "-")
-    fuzz_options(num, "u", "+")
-    fuzz_options(num, "x", "#")
-
-
-# long int, int, float
-def bit_32():
-    a = format(random.getrandbits(32), '032b')
-    num = int(a, 2)
-    fuzz(num, "f")
-
-# double
-def bit_64():
-    a = format(random.getrandbits(64), '064b')
-    num = int(a, 2)
-    fuzz(num, "lf")
-    fuzz(num, "lg")
-    fuzz(num, "le")
 
 def main():
 
-    s = "  \" Hello World \\n \" "
-    helloTest(s)     #21.80% coverage
-    intTest()        #23.71%
-    uintTest()
-    hexTest()
-    longTest()
-    longlongTest()
-    longdoubleTest()
-    s = " ' d ' "
-    charTest(s)
-    doubleTest()
-    floatTest()
-    ushortTest()
-    shortTest()
-    char_fuzz()    
-    u_short_int()    
-    short_int()
-    int_test = int_fuzz()
-    fuzz(int_test, "i")   
-    uint_test = sm_int_fuzz()
-    fuzz(uint_test, "lu") 
-    bit_16()
-    bit_32()
-    bit_64() #40.87% (Gotten 42.51%)
+    fuzz1()
 
     print os.system("gcov vfprintf.c")
 
 if __name__ == "__main__":
     main()
-
-#flags -> -, +, #, 0
--- fuzzer/toddjohnson/test-printf.c
+++ fuzzer/toddjohnson/test-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include <math.h>
 #include "musl.h"
 #define LEN 10000
 char buf[LEN];
 int main(int argc, const char *argv[]) {
-musl_snprintf(buf, LEN, "%le" , 10274504855482014168);
+musl_snprintf(buf, LEN, "%c %d %f %e %s %d %i %x %u %o %g %% %a %l %ll %p" , 'W',14,340.563716465,12345678.0,"njyTfYvPOq",14,14,0x9f3aL,14,013463373556L,14,'%',0xc,14,14,14);
 printf("%s", buf);return 0;
 }
