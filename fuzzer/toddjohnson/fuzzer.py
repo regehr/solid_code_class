538b834ca4c67b9da0f9663331be250794a00b76
Todd Johnson <tojohnso@lab1-3.eng.utah.edu>
2014-04-02 20:22:24 -0600

    Updated with final coverage and pdf doc for Thursday.

--- fuzzer/toddjohnson/fuzzer.py
+++ fuzzer/toddjohnson/fuzzer.py
@@ -7,6 +7,13 @@ import filecmp
 import decimal
 from random import randrange
 
+
+#I tried over and over to break all the fuzzing cases 
+#into smaller chunks but my code coverage was dimishing
+# as i added more and more cases. This is the only thing
+#i could get to truly work. I find this interesting
+#and i will definately continue to work on fuzzing.
+
 def large_string(n):
     ret = ""
     for i in xrange(n):
@@ -16,8 +23,8 @@ def large_string(n):
 def random_int(n):
     return random.getrandbits(n)
 
-def fuzz1():
-    typ = "c %d %f %e %s %d %i %x %u %o %g %% %a %l %ll %p"
+def fuzz_all():
+    typ = "c %03d %c %.3f %6.3f %e %5.3s %.d %+i %#x %u %o %g %% %a %-l % ll %p %ls %+ld %lu %5lf %-lg %le %h %#010X %z %i %i %l %ll %llu %hu %c %i %i %c %s"
     c = "'" + random.choice(string.letters) + "'"
     d = random_int(8)
     f = random.uniform(1.0, 1000.1)
@@ -28,7 +35,15 @@ def fuzz1():
     x = hex(random_int(16))
     o = oct(random_int(32))
     sym = "'%'"
-    dH = hex(12)
+    dH = hex(random_int(8))
+    h = 3
+    lg = 3.141592
+    maximum = 2**32 - 1
+    minimum = -2**32 - 1
+    l = -12
+    uc = 65+4
+    m = " 'c ' + 65 "
+    s2 = "  \" Hello World \\n \" "
 
     file = open("test-printf.c", "w")
     file.write("#include <stdio.h>\n")
@@ -38,7 +53,7 @@ def fuzz1():
     file.write("#define LEN 10000\n")
     file.write("char buf[LEN];\n")
     file.write("int main(int argc, const char *argv[]) {\n")
-    file.write("musl_snprintf(buf, LEN, \"%" +  typ +  "\" , " + str(c) + "," + str(d) + "," +str(f) + "," +str(e) + "," +str(s) + "," +str(d) +  "," +str(i) +  "," + str(x) +  "," + str(i)+  "," +str(o) + "," +str(d) +"," +str(sym) +  "," + str(dH) +  "," + str(d) +  "," + str(d) +  "," + str(d) + ");\n")
+    file.write("musl_snprintf(buf, LEN, \"%" + typ +  "\" , " + str(c) + "," + str(d) + "," + str(i) + "," +str(f) + "," +str(f) + "," +str(e) + "," +str(s) + "," +str(d) +  "," +str(i) +  "," + str(x) +  "," + str(i)+  "," +str(o) + "," +str(d) +"," +str(sym) +  "," + str(dH) +  "," + str(d) +  "," + str(d) +  "," + str(d) +  "," + str(h) +  "," + str(d) +  "," + str(d) +  "," + str(f) +  "," + str(lg) +  "," + str(e) +  "," + str(h) +  "," + str(x) + "," + str(i) +  "," + str(i)  + "," + str(i) +  "," + str(l) +  "," + str(l)  +  "," + str(d) +  "," + str(d)+  "," + str(uc)+  "," + str(maximum)+  "," + str(minimum)+ "," + str(m)+  "," + str(s2) +");\n")
     file.write("printf(\"%s\", buf);")
     file.write("return 0;\n}")
     file.close()
@@ -46,7 +61,7 @@ def fuzz1():
 
 def main():
 
-    fuzz1()
+    fuzz_all()
 
     print os.system("gcov vfprintf.c")
 
--- fuzzer/toddjohnson/test-printf.c
+++ fuzzer/toddjohnson/test-printf.c
@@ -5,6 +5,6 @@
 #define LEN 10000
 char buf[LEN];
 int main(int argc, const char *argv[]) {
-musl_snprintf(buf, LEN, "%c %d %f %e %s %d %i %x %u %o %g %% %a %l %ll %p" , 'W',14,340.563716465,12345678.0,"njyTfYvPOq",14,14,0x9f3aL,14,013463373556L,14,'%',0xc,14,14,14);
+musl_snprintf(buf, LEN, "%c %03d %c %.3f %6.3f %e %5.3s %.d %+i %#x %u %o %g %% %a %-l % ll %p %ls %+ld %lu %5lf %-lg %le %h %#010X %z %i %i %l %ll %llu %hu %c %i %i %c %s" , 'e',7,14,330.517733558,330.517733558,12345678.0,"vkzubBeesu",7,14,0x7ff9L,14,026152006227L,7,'%',0xbaL,7,7,7,3,7,7,330.517733558,3.141592,12345678.0,3,0x7ff9L,14,14,14,-12,-12,7,7,69,4294967295,-4294967297, 'c ' + 65 ,  " Hello World \n " );
 printf("%s", buf);return 0;
 }
