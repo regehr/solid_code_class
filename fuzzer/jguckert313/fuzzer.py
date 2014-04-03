42552f7c00ab0f0701b22cf49488938a4a6c998b
Jake Guckert <jguckert313@gmail.com>
2014-04-02 23:35:04 -0600

    achieving ~50% code coverage.

--- fuzzer/jguckert313/fuzzer.py
+++ fuzzer/jguckert313/fuzzer.py
@@ -2,36 +2,55 @@ import random
 import subprocess
 import string	
 
-def fuzzInts():
-	f = open("test_printf.c", "w")
-	f.write("#include <stdio.h>\n")
-	f.write("#include \"musl.h\"\n")
-	f.write("#define LEN 10000\n")
-	f.write("int main() {\r")
-	f.write("char buf[LEN];\n")
-	
-	for i in range(1, 10000):
-		d = random.randint(1,10000)
-		f.write("\tmusl_snprintf (buf, LEN, \"%d"  "\", " + str(d) + ");\n")
-	
-	f.write("return 0;\r}")
-	f.close
+# used to fuzz %d, %o, %x, %X, %f, %e, %E
+def fuzz_nums():
+	f = open("test_printf.c", "a")
+	for i in range(1, 1000):
+		n = str(random.randint(1,10000))
+		f.write("\tmusl_snprintf (buf, LEN, \"%d"  "\", " + n + ");\n")
+		f.write("\tmusl_snprintf (buf, LEN, \"%o"  "\", " + n + ");\n")
+		f.write("\tmusl_snprintf (buf, LEN, \"%x"  "\", " + n + ");\n")
+		f.write("\tmusl_snprintf (buf, LEN, \"%X"  "\", " + n + ");\n")
+		f.write("\tmusl_snprintf (buf, LEN, \"%#o"  "\", " + n + ");\n")
+		f.write("\tmusl_snprintf (buf, LEN, \"%#x"  "\", " + n + ");\n")
+		f.write("\tmusl_snprintf (buf, LEN, \"%#X"  "\", " + n + ");\n")
+		f.write("\tmusl_snprintf (buf, LEN, \"%e"  "\", " + n + ");\n")
+		f.write("\tmusl_snprintf (buf, LEN, \"%E"  "\", " + n + ");\n")
+		f.write("\tmusl_snprintf (buf, LEN, \"%f"  "\", " + n + ");\n")
+		f.write("\tmusl_snprintf (buf, LEN, \"%l"  "\", " + n + ");\n")
+		f.write("\tmusl_snprintf (buf, LEN, \"%ll"  "\", " + n + ");\n")
+		f.write("\tmusl_snprintf (buf, LEN, \"%h"  "\", " + n + ");\n")
+		f.write("\tmusl_snprintf (buf, LEN, \"%hh"  "\", " + n + ");\n")
+		f.write("\tmusl_snprintf (buf, LEN, \"%L"  "\", " + n +"L" +");\n")
 
-#def fuzzChars():
-#	f = open("test_printf.c", "w")
-#	f.write("#include <stdio.h>\n")
-#	f.write("#include \"musl.h\"\n")
-#	f.write("int main() {\r")
-	
-#	for i in range(1, 10000):
-#		char = random.choice(string.ascii_lowercase)
-#		f.write("musl_snprintf (\"%c"  "\", " + char + ");\n")f
-	
-#	f.write("return 0;\r}")
-#	f.close
+
+	f.close()
+
+# used to fuzz %c, %s
+def fuzz_chars():
+	f = open("test_printf.c", "a")
+	s = ""
+	for i in range(1, 1000):
+		c = random.choice(string.lowercase)
+		s += c
+		f.write("\tmusl_snprintf (buf, LEN, \"%c"  "\", " + "\"" + c + "\"" + ");\n")
+		f.write("\tmusl_snprintf (buf, LEN, \"%s"  "\", " + "\"" + s + "\"" + ");\n")
+
 
 def main():
-	fuzzInts()
-	#fuzzChars()
+	f = open("test_printf.c", "w")
+	f.write("#include <stdio.h>\n")
+	f.write("#include <stdarg.h>\n")
+	f.write("#include \"musl.h\"\n")
+	f.write("\n#define LEN 1000\n")
+	f.write("char buf[LEN];\n")
+	f.write("\nint main() {\r")
+	f.close()
+	fuzz_nums()
+	fuzz_chars()
+	f = open("test_printf.c", "a")
+	f.write("\n\treturn 0;\n")
+	f.write("}")
 
 main()
+#ulong, ullong, short, ushort
