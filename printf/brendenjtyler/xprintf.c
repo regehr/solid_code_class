a32717a248a0a9a923c71fb87d69e74b04bd7774
Brenden Tyler <brendenjtyler@gmail.com>
2014-04-14 22:33:23 -0600

    Added make file and parsing for width and padding
    Now parses out the zero-padding and width options. They don't affect the
    output yet but I can get them.

--- printf/brendenjtyler/xprintf.c
+++ printf/brendenjtyler/xprintf.c
@@ -2,25 +2,21 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <math.h>
 
-
+char *utoa(unsigned int, char*);
 char *itoa(int, char *, int);
 void xprintf(const char *, ...);
-
-char *utoa(unsigned int toConvert, char* buf)
-{
-  sprintf(buf, "%u", toConvert);
-  return buf;
-}
+void getArgs(int *, const char *);
 
 int main (int argc, char** argv)
 {
   int *chars;
   unsigned int testInt = 3294967295;
-  xprintf("this%n is a test string, %d, %u\n", chars, 5, testInt);
-  xprintf("%d\n", *chars);
-  printf("this%n is a test string, %d, %u\n", chars, 5, testInt);
+  printf("this%n is a test string, %10d, %u\n", chars, 5, testInt);
   printf("%d\n", *chars);
+  xprintf("this%n is a test string, %010d, %u\n", chars, 5, testInt);
+  xprintf("%d\n", *chars);
 
   return 0;
 }
@@ -39,7 +35,9 @@ void xprintf(const char *fmt, ...)
   int *soFar;
   char *s;
   char fmtbuf[256];
-  
+  bool padWithZeros = false;
+  int width = 0;
+
   //start the va_arg stuff
   va_start(argp, fmt);
   
@@ -53,15 +51,9 @@ void xprintf(const char *fmt, ...)
 	  putchar(*p);
 	  continue;
 	}
-      /*
-       * support the following flag:
-       * 0
-       *
-       * also, Josh wants this discussed separately:
-       * field width digit string
-       */
       switch(*++p)
 	{
+	  //print the character
 	case 'c':
 	  i = va_arg(argp, int);
 	  putchar(i);
@@ -74,6 +66,7 @@ void xprintf(const char *fmt, ...)
 	  fputs(s, stdout);
 	  break;
 	  
+	  //print the number of characters printed so far
 	case 'n':
 	  soFar = va_arg(argp, int*);
 	  *soFar = p-fmt-1;  //minus one because we don't count this character
@@ -85,12 +78,14 @@ void xprintf(const char *fmt, ...)
 	  fputs(s, stdout);
 	  break;
 	  
+	  //print an unsigned int
 	case 'u':
 	  u = va_arg(argp, unsigned int);
 	  s = utoa(u, fmtbuf);
 	  fputs(s, stdout);
 	  break;
 
+	  //print the int in hex
 	case 'x':
 	  i = va_arg(argp, int);
 	  s = itoa(i, fmtbuf, 16);
@@ -101,9 +96,45 @@ void xprintf(const char *fmt, ...)
 	case '%':
 	  putchar('%');
 	  break;
+
+	  //
+	default:
+
+	  //check to see if we want to pad with zeros
+	  if(*p == '0')
+	    {
+	      padWithZeros = true;
+	      p++;
+	    }
+
+	  //process all of the extra args
+	  getArgs(&width, p);
+	  break;
 	}
     }
   va_end(argp);
+}
+
+void getArgs(int * width, const char *p)
+{      
+  //see how many extra things to print out
+  *width += atoi(p);
+  
+  if(*width == 0)
+    {
+      return;
+    }
+  else
+    {
+      p += (int)(floor (log10 (abs (*width))));
+    }
+}
+
+/* converts an unsigned int into a string */
+char *utoa(unsigned int toConvert, char* buf)
+{
+  sprintf(buf, "%u", toConvert);
+  return buf;
 }
 
 /* A utility function to reverse a string  */
