f4899b0b55e2cef0bc99985191805777ef280ce5
Josh Kunz <joshkunz@me.com>
2014-04-15 11:39:51 -0600

    %d implemented.

--- underhanded_printf/joshkunz/test.c
+++ underhanded_printf/joshkunz/test.c
@@ -1,6 +1,12 @@
 #include "xprintf.h"
+#include <stdio.h>
 
 int main() {
-    xprintf("%s %d%%%cthis is a test.\n", "Really.", 10, 'b');
+    printf("%d\n", -13 % 10);
+    xprintf("Should print '%%': %%\n"
+            "Should print  'b': %c\n"
+            "Should print 'Hello': %s\n"
+            "Should print '13': %d\n"
+            "Should print '-13': %d\n", 'b', "Hello", 13, -13);
     return 0;
 }
--- underhanded_printf/joshkunz/xprintf.c
+++ underhanded_printf/joshkunz/xprintf.c
@@ -4,19 +4,53 @@
 #include <stdio.h>
 #include <assert.h>
 #include <stdbool.h>
+#include <limits.h>
+
+/* Macros for stringifying the values of other macros. */
+#define __STRINGIFY(x) (#x)
+#define STRINGIFY(x) (__STRINGIFY(x))
 
 #define UNREACHABLE assert(false && "Unreachable point.")
+
+#define EXPRINTF (-1)
+
+#define INT_MAXSTRLEN (sizeof(STRINGIFY(INT_MIN)))
+#define UINT_MAXSTRLEN (sizeof(STRINGIFY(UINT_MAX)))
+
+void reverses(char * out, int nchars) {
+    for (int i = 0; i < (nchars / 2); i++) {
+        char tmp = out[i];
+        out[i] = out[(nchars - 1) - i];
+        out[(nchars - 1) - i] = tmp;
+    }
+}
+
+int itos(int v, char * out) {
+    char * _out = out;
+    int nchars = 1;
+    bool is_neg = v < 0;
+    v = abs(v);
+    for (; v > 10; v /= 10, nchars++) {
+        *_out++ = '0' + (v % 10);
+    }
+    *_out++ = '0' + v;
+    if (is_neg) { *_out = '-'; nchars++; }
+    reverses(out, nchars);
+    return nchars;
+}
 
 int xvfprintf(FILE * stream, const char * format, va_list args) {
     int count = 0;
     for (; *format; format++) {
         if (*format != '%') { 
-            if (fputc(*format, stream) != *format) { return -1; }
+            if (fputc(*format, stream) != *format) { return EXPRINTF; }
             continue; }
         switch (*(format + 1)) {
-            case 'd':
-                UNREACHABLE;
-                break;
+            case 'd': {
+                char str[INT_MAXSTRLEN] = { '\0' };
+                int nchars = itos(va_arg(args, int), str);
+                if (fwrite(str, nchars, 1, stream) < 1) { return EXPRINTF; }
+                break; }
             case 'u':
                 UNREACHABLE;
                 break;
@@ -25,17 +59,17 @@ int xvfprintf(FILE * stream, const char * format, va_list args) {
                 break;
             case 's': {
                 char * str = (char *) va_arg(args, char *);
-                if (fwrite(str, strlen(str), 1, stream) < 1) { return -1; }
+                if (fwrite(str, strlen(str), 1, stream) < 1) { return EXPRINTF; }
                 break; }
             case 'c': {
                 int ch = (char) va_arg(args, int);
-                if (fputc(ch, stream) != ch) { return -1; } 
+                if (fputc(ch, stream) != ch) { return EXPRINTF; } 
                 break; }
             case 'n':
                 UNREACHABLE;
                 break;
             case '%':
-                if (fputc('%', stream) != '%') { return -1; }
+                if (fputc('%', stream) != '%') { return EXPRINTF; }
                 break;
         }
         format++;
