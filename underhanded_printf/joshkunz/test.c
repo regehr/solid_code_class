c8a08ab89cebc809d9b5586d8cb53f770ee3c803
Josh Kunz <joshkunz@me.com>
2014-04-15 18:46:00 -0600

    Added field width padding.

--- underhanded_printf/joshkunz/test.c
+++ underhanded_printf/joshkunz/test.c
@@ -4,16 +4,16 @@
 
 int main() {
     int x = -1;
-    xprintf("abc%n"
-            "Should print '%%': %%\n"
-            "Should print  'b': %c\n"
-            "Should print 'Hello': %s\n"
-            "Should print '13': %d\n"
-            "Should print '-13': %d\n"
-            "Should print '-2147483648': %d\n"
-            "Should print '3242342142': %u\n"
-            "Should print '1234cde': %x\n",
+    xprintf("abc%n (for %%n modfier)\n"
+            "Should print '%%': '%%'\n"
+            "Should print '  b': '%3c'\n"
+            "Should print 'Hello': '%s'\n"
+            "Should print '  13': '%4d'\n"
+            "Should print '-13': '%d'\n"
+            "Should print '-2147483648': '%d'\n"
+            "Should print '3242342142': '%u'\n"
+            "Should print '1234cde': '%x'\n",
             &x, 'b', "Hello", 13, -13, INT_MIN, 3242342142U, 0x1234cdeU);
-    printf("Should print '3': %d\n", x);
+    printf("Should print (%%n) '3': '%d'\n", x);
     return 0;
 }
--- underhanded_printf/joshkunz/xprintf.c
+++ underhanded_printf/joshkunz/xprintf.c
@@ -5,19 +5,16 @@
 #include <assert.h>
 #include <stdbool.h>
 #include <limits.h>
-
-/* Macros for stringifying the values of other macros. */
-#define __STRINGIFY(x) (#x)
-#define STRINGIFY(x) (__STRINGIFY(x))
+#include <math.h>
+#include <ctype.h>
 
 #define UNREACHABLE assert(false && "Unreachable point.")
-
 #define EXPRINTF (-1)
 
-#define INT_MAXSTRLEN (sizeof(STRINGIFY(INT_MIN)))
-#define UINT_MAXSTRLEN (sizeof(STRINGIFY(UINT_MAX)))
+#define MAXSTRLEN (((int) ceil(log10((double) UINT_MAX))) + 1)
 
 void reverses(char * out, int nchars) {
+    assert(nchars >= 0);
     for (int i = 0; i < (nchars / 2); i++) {
         char tmp = out[i];
         out[i] = out[(nchars - 1) - i];
@@ -26,6 +23,7 @@ void reverses(char * out, int nchars) {
 }
 
 char cfori(int i) {
+    assert(i >= 0 && i <= 32);
     return i < 10 ? '0' + i : 'a' + (i - 10);
 }
 
@@ -38,64 +36,102 @@ int utos(unsigned int v, char * out, int radix, bool is_neg) {
     *_out++ = cfori(v);
     if (is_neg) { *_out = '-'; nchars++; }
     reverses(out, nchars);
+    out[nchars] = '\0';
     return nchars;
+}
+
+unsigned int xabs(int i) {
+    return i == INT_MIN
+        ? ((unsigned int) INT_MAX) + 1
+        : (unsigned int) abs(i);
 }
 
 int itos(int v, char * out) {
     /* Note handle INT_MIN case. */
-    unsigned int abs_v = v == INT_MIN
-                                ? ((unsigned int) INT_MAX) + 1
-                                : (unsigned int) abs(v);
-    int nchars = utos(abs_v, out, 10, v < 0);
+    int nchars = utos(xabs(v), out, 10, v < 0);
     return nchars;
+}
+
+int field_width(const char ** fmt) {
+    const char * format = *fmt;
+    int fw = 0;
+    for (; *format && isdigit(*format); format++) {
+        fw *= 10;
+        fw += *format - '0';
+    }
+    *fmt = format;
+    return fw;
+}
+
+char * pad(int c, int fw, int len, char * fwb, char * str) {
+    memset(fwb, c, fw);
+    fwb[fw] = '\0';
+    if (fw > len) {
+        memcpy(&fwb[fw] - len, str, len);
+        return fwb;
+    } else {
+        return str;
+    }
 }
 
 int xvfprintf(FILE * stream, const char * format, va_list args) {
     int count = 0;
+    char str[MAXSTRLEN];
+    memset(str, '\0', MAXSTRLEN);
+    char * str_out = NULL;
+    int nchars = -1;
+
     for (; *format; format++) {
         if (*format != '%') { 
             count++;
             if (fputc(*format, stream) != *format) { return EXPRINTF; }
             continue; 
         }
-        switch (*(format + 1)) {
-            case 'd': {
-                char str[INT_MAXSTRLEN] = { '\0' };
-                int nchars = itos(va_arg(args, int), str);
-                count += nchars;
-                if (fwrite(str, nchars, 1, stream) < 1) { return EXPRINTF; }
-                break; }
-            case 'u': {
-                char str[UINT_MAXSTRLEN] = { '\0' };
-                int nchars = utos(va_arg(args, unsigned int), str, 10, false);
-                count += nchars;
-                if (fwrite(str, nchars, 1, stream) < 1) { return EXPRINTF; }
-                break; }
-            case 'x': {
-                char str[UINT_MAXSTRLEN] = { '\0' };
-                int nchars = utos(va_arg(args, unsigned int), str, 16, false);
-                count += nchars;
-                if (fwrite(str, nchars, 1, stream) < 1) { return EXPRINTF; }
-                break; }
-            case 's': {
-                char * str = (char *) va_arg(args, char *);
-                count += strlen(str);
-                if (fwrite(str, strlen(str), 1, stream) < 1) { return EXPRINTF; }
-                break; }
-            case 'c': {
-                int ch = (char) va_arg(args, int);
-                count++;
-                if (fputc(ch, stream) != ch) { return EXPRINTF; } 
-                break; }
+
+        format++;
+        int fw = field_width(&format);
+        char fwb[fw + 1];
+        //printf("Got fw: %d\n",  fw);
+
+        switch (*format) {
+            case 'd':
+                nchars = itos(va_arg(args, int), str);
+                str_out = pad(' ', fw, nchars, fwb, str);
+                break;
+            case 'u':
+                nchars = utos(va_arg(args, unsigned int), str, 10, false);
+                str_out = pad(' ', fw, nchars, fwb, str);
+                break;
+            case 'x': 
+                nchars = utos(va_arg(args, unsigned int), str, 16, false);
+                str_out = pad(' ', fw, nchars, fwb, str);
+                break;
+            case 's': 
+                str_out = (char *) va_arg(args, char *);
+                str_out = pad(' ', fw, strlen(str_out), fwb, str_out);
+                break;
+            case 'c':
+                str[0] = (char) va_arg(args, int);
+                str_out = pad(' ', fw, 1, fwb, str);
+                break;
             case 'n':
                 *(va_arg(args, int *)) = count;
                 break;
             case '%':
-                count++;
-                if (fputc('%', stream) != '%') { return EXPRINTF; }
+                str[0] = '%';
+                str_out = pad(' ', fw, 1, fwb, str);
                 break;
         }
-        format++;
+        if (str_out == NULL) { str_out = str; }
+
+        count += strlen(str_out);
+        if (strlen(str_out) > 0 && 
+                fwrite(str_out, strlen(str_out), 1, stream) < 1) { 
+            return EXPRINTF; 
+        }
+
+        memset(str, '\0', MAXSTRLEN);
+        str_out = NULL;
     }
     return count;
 }
