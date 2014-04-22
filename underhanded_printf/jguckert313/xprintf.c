905079f7b294a6d3fb09d3ce0b17defe6fd42743
Jake Guckert <jguckert313@gmail.com>
2014-04-22 11:42:19 -0600

    xprintf working for everything except %u

--- underhanded_printf/jguckert313/xprintf.c
+++ underhanded_printf/jguckert313/xprintf.c
@@ -1,13 +1,74 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-extern char *itoa(int, char *, int);
+void swap(char c1, char c2) {
+    char c3 = c1;
+    c1 = c2;
+    c2 = c3;
+}
 
-void xprintf(const char *fmt, ...) {
+
+/* reverse() and itoa referenced from http://www.geeksforgeeks.org/implement-itoa/ */
+void reverse(char str[], int length)
+{
+    int start = 0;
+    int end = length -1;
+    while (start < end)
+    {
+        swap(*(str+start), *(str+end));
+        start++;
+        end--;
+    }
+}
+
+// Implementation of itoa()
+char* itoa(int num, char* str, int base)
+{
+    int i = 0;
+    bool isNegative = false;
+ 
+    /* Handle 0 explicitly, otherwise empty string is printed for 0 */
+    if (num == 0)
+    {
+        str[i++] = '0';
+        str[i] = '\0';
+        return str;
+    }
+ 
+    // In standard itoa(), negative numbers are handled only with 
+    // base 10. Otherwise numbers are considered unsigned.
+    if (num < 0 && base == 10)
+    {
+        isNegative = true;
+        num = -num;
+    }
+ 
+    // Process individual digits
+    while (num != 0)
+    {
+        int rem = num % base;
+        str[i++] = (rem > 9)? (rem-10) + 'a' : rem + '0';
+        num = num/base;
+    }
+ 
+    // If number is negative, append '-'
+    if (isNegative)
+        str[i++] = '-';
+ 
+    str[i] = '\0'; // Append string terminator
+ 
+    // Reverse the string
+    reverse(str, i);
+ 
+    return str;
+}
+
+int xprintf(const char *fmt, ...) {
     const char *p;
     va_list argp;
-    int i;
+    int i, bytes_printed = 0;
     char *s;
     char fmtbuf[256];
     
@@ -23,11 +84,13 @@ void xprintf(const char *fmt, ...) {
             case 'c':
                 i = va_arg(argp, int);
                 putchar(i);
+                bytes_printed += 1;
                 break;
                 
             case 'd':
                 i = va_arg(argp, int);
                 s = itoa(i, fmtbuf, 10);
+                bytes_printed += 1;
                 fputs(s, stdout);
                 break;
                 
@@ -55,9 +118,16 @@ void xprintf(const char *fmt, ...) {
 	}
     
     va_end(argp);
+    return bytes_printed;
 }
 
 int main() {
-    xprintf("%d\n", 12);
+    xprintf("%d\n", 21);
+    xprintf("%s\n", "hello");
+    xprintf("%x\n", 100);
+    xprintf("%c\n", 'a');
+    xprintf("%%\n", 0);
+    xprintf("%n\n", 0);
+    xprintf("%u", 12);
     return 0;
 }
